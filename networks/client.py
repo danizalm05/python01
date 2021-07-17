9fd1233b3f804486dbf5c9fa2e02035135e12815
danizalm05 <danizalm05@gmail.com>
2021-07-17 16:08:06 +0300

    1 fix

--- networks/client.py
+++ networks/client.py
@@ -7,7 +7,7 @@ http://www.lamed-oti.com/school/rs/networks/
 download 'echo_server_stream.pyc'
 run: python  echo_server_stream.pyc
 '''
-my_socket = socket.socket()
+my_socket =      socket.socket()
 my_socket.connect(('127.0.0.1', 8820))
 cmd = input("Your command? ")
 #msg = "Hell"
