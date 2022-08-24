7134c8ef26db66a217895013943d1f8b6c1dc524
Richm <ric5hme45@yahoo.com>
2022-08-24 12:27:18 +0300

    14045

--- PyQt/intro/todo.py
+++ PyQt/intro/todo.py
@@ -4,5 +4,5 @@ https://www.youtube.com/watch?v=EFKI9bu4lrY&list=PLCC34OHNcOtpmCA8s_dpPMvQLyHbvx
 https://github.com/flatplanet/pyqt5_youtube_playlist/blob/main/todo.py
 
 https://github.com/flatplanet/pyqt5_youtube_playlist
-01:18
+05:00
 """
