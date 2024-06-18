efb08210c9fb077d74af638b8aa7f49c47f97acc
danizalm05 <danizalm05@gmail.com>
2024-06-18 16:23:33 +0300

    5250

--- opencv/murtazaNew/03-Resizing.py
+++ opencv/murtazaNew/03-Resizing.py
@@ -8,30 +8,32 @@ https://github.com/murtazahassan/Learn-OpenCV-in-3-hours/blob/master/chapter3.py
 import numpy as np
 import cv2
 import getpass
+import os
 
 frameWidth = 640
 frameHeight = 480
 BASE_FOLDER = 'C:/Users/'+ getpass.getuser()
 
-
+file_name = '2.jpg'
 BASE_FOLDER = BASE_FOLDER +'/Pictures/Saved Pictures/'
-path = BASE_FOLDER+'road.jpg'  #'b1.jpg' 'lena.png' 'bb.jpg'
+path = BASE_FOLDER+file_name  
 print(path)
 
-img = cv2.imread(path)
-print(img.shape)
 
+if os.path.isfile(path):
+   img = cv2.imread(path)
+   print("img.shape ",img.shape)
+   imgResize = cv2.resize(img,(1000,500))
+   print("imgResize.shape",imgResize.shape)
+   imgCropped = img[46:219,252:495]
+   cv2.imshow("Image Resize",imgResize)
+   cv2.imshow("Image",img)
+   cv2.imshow("Image Cropped",imgCropped)
 
-imgResize = cv2.resize(img,(1000,500))
-print(imgResize.shape)
-imgCropped = img[46:219,252:495]
+   cv2.waitKey(0)
 
-cv2.imshow("Image Resize",imgResize)
-cv2.imshow("Image",img)
-cv2.imshow("Image Cropped",imgCropped)
-
-cv2.waitKey(0)
-
+else:
+    print("ERROR --> Missing File: " + path   ) 
 
 
 
