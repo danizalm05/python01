ebf007f801bb39301db2beb911d92089049a186f
danizalm05 <danizalm05@gmail.com>
2024-06-19 16:35:35 +0300

    27607

--- opencv/murtazaNew/04-text.py
+++ opencv/murtazaNew/04-text.py
@@ -8,6 +8,7 @@ https://github.com/murtazahassan/Learn-OpenCV-in-3-hours/blob/master/chapter4.py
 import numpy as np
 import cv2
 import getpass
+import os
 
 frameWidth = 640
 frameHeight = 480
@@ -22,27 +23,22 @@ cv2.imshow("Image",img)
 cv2.waitKey(0)
 print(img)
 
-BASE_FOLDER = 'C:/Users/'+ getpass.getuser()
+BASE_FOLDER = 'C:/Users/'+ getpass.getuser() +'/Pictures/Saved Pictures/'
+file_name = '2seef2.jpg'
+path = BASE_FOLDER + file_name   
 
 
-BASE_FOLDER = BASE_FOLDER +'/Pictures/Saved Pictures/'
-path = BASE_FOLDER+'road.jpg'  #'b1.jpg' 'lena.png' 'bb.jpg'
 print(path)
-
-img = cv2.imread(path)
-print(img.shape)
-
-
-
-cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
-cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
-cv2.circle(img,(400,50),30,(255,255,0),5)
-cv2.putText(img," OPENCV  ",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),3)
-
-
-cv2.imshow("Image",img)
-
-cv2.waitKey(0)
-
+if os.path.isfile(path):
+     img = cv2.imread(path)
+     print(img.shape)
+     cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+     cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
+     cv2.circle(img,(400,50),30,(255,255,0),5)
+     cv2.putText(img,"OPENCV",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),3)
+     cv2.imshow("Image",img)
+     cv2.waitKey(0)
+else:
+    print("ERROR --> Missing File: " + path   ) 
 
 
--- opencv/murtazaNew/05-Perspctive.py
+++ opencv/murtazaNew/05-Perspctive.py
@@ -6,28 +6,33 @@ https://github.com/murtazahassan/Learn-OpenCV-in-3-hours/blob/master/chapter5.py
 import numpy as np
 import cv2
 import getpass
+import os
 
-BASE_FOLDER = 'C:/Users/'+ getpass.getuser()
-BASE_FOLDER = BASE_FOLDER +'/Pictures/Saved Pictures/'
-path = BASE_FOLDER+'cards.jpg'  #'b1.jpg' 'lena.png' 'bb.jpg'
+image_name =  'cards.jpg' 
+BASE_FOLDER = 'C:/Users/'+ getpass.getuser()+'/Pictures/Saved Pictures/'
+path = BASE_FOLDER + image_name  #'b1.jpg' 'lena.png' 'bb.jpg'
 print(path)
 
-img = cv2.imread(path)
-print(img.shape)
 
-cv2.imshow("Image",img)
-width,height = 250,350
-# 4  conrners of the card
-pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
-#4 corners of the output screen
-pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
-matrix = cv2.getPerspectiveTransform(pts1,pts2)
-imgOutput = cv2.warpPerspective(img,matrix,(width,height))
+if os.path.isfile(path):
+    img = cv2.imread(path)
+    print(img.shape)
 
-print(pts1)
-cv2.imshow("Image",img)
-cv2.imshow("Output",imgOutput)
-cv2.waitKey(0)
+    cv2.imshow("Image",img)
+    width,height = 250,350
+    # 4  conrners of the card
+    pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
+    #4 corners of the output screen
+    pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgOutput = cv2.warpPerspective(img,matrix,(width,height))
 
+    print(pts1)
+    cv2.imshow("Image",img)
+    cv2.imshow("Output",imgOutput)
+    cv2.waitKey(0)
+
+else:
+    print("ERROR --> Missing File: " + path   ) 
 
 
