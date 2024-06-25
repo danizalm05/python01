6d692f02c174e14b58dfd5503248ea0d81a6e099
Richm <ric5hme45@yahoo.com>
2024-06-25 12:28:57 +0300

    988

--- networks/cyber eduction center/client.py
+++ networks/cyber eduction center/client.py
@@ -1,23 +1,22 @@
 import socket
 
 '''
-רשתות מחשבים  page 40 exer  2.1
-run server.py before runing  this program 
-https://cyber.org.il/data/
+https://cyber.org.il/data/  page 40   2.1
+run 'server.py' before running  this program 
+
 nslookup  networks.cyber.org.il ===> ip= 213.57.2.5
-http://cyber.org.il/
+ 
 python -V    python version:   Python 3.6.4 :: Anaconda, Inc.
 http://www.lamed-oti.com/school/rs/networks/
-download 'echo_server_stream.pyc'
-run: python  echo_server_stream.pyc
+ 
+ 
 '''
-my_socket =      socket.socket()
+my_socket = socket.socket()
 my_socket.connect(('127.0.0.1', 8820))
 cmd = input("Your command? ")
-#msg = "Hell"
-my_socket.send(cmd.encode())
+my_socket.send(cmd.encode())# encode:  turn type string to type bytes '
 
 
-data = my_socket.recv(1024).decode()
+data = my_socket.recv(1024).decode()# decode opposite to encode
 print ('The server sent: ' + (data))
 my_socket.close()
--- networks/cyber eduction center/server.py
+++ networks/cyber eduction center/server.py
@@ -2,6 +2,7 @@ import socket
 server_socket = socket.socket()
 server_socket.bind(('127.0.0.1', 8820))# bind the socket to host and port
 server_socket.listen(1) #1 == so that it will perpetually listen till we close the connection
+print("Server is up and running")
 (client_socket, client_address) = server_socket.accept()
 print ("Connection from: " + str(client_address))
 client_name = client_socket.recv(1024).decode()
