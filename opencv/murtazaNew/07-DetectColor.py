2d58273ef7dcf7b71eceb49d7c83a2b6087e6c27
danizalm05 <danizalm05@gmail.com>
2024-06-26 17:59:21 +0300

    20200

--- opencv/murtazaNew/07-DetectColor.py
+++ opencv/murtazaNew/07-DetectColor.py
@@ -66,28 +66,33 @@ cv2.createTrackbar("Hue Max","TrackBars",70,179,empty)
 cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
 cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
 cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
-cv2.createTrackbar("Val Max","TrackBars",250,255,empty)
-
+cv2.createTrackbar("Val Max","TrackBars",200,255,empty)
+img = cv2.imread(path)
 while True:
-    img = cv2.imread(path)
+   
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    #        hue
     h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
     h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    #        saturation
     s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
     s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    #        value
     v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
     v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
 
-    print(h_min,h_max,s_min,s_max,v_min, v_max)
-    lower = np.array([h_min,s_min,v_min])
+    #print(h_min,h_max,s_min,s_max,v_min, v_max)
+    lower = np.array([h_min,s_min,v_min])#array of 3 min values
     upper = np.array([h_max,s_max,v_max])
+    print(lower)
     mask = cv2.inRange(imgHSV,lower,upper)
     imgResult = cv2.bitwise_and(img,img,mask=mask)
 
 
     #cv2.imshow("Image", img)
     #cv2.imshow("imgHSV", imgHSV)
-    imgStack = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
+    imgStack = stackImages(0.5,([img,imgHSV],[mask,imgResult]))
+    #all white values in mask will appear in the result
     cv2.imshow("Result", imgResult)
     cv2.imshow("Stacked Images", imgStack)
 
