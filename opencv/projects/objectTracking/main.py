a64c72ce900646e3462ac5e88e79c59297ab6803
Richm <ric5hme45@yahoo.com>
2024-09-01 12:29:57 +0300

    17828

--- opencv/projects/objectTracking/main.py
+++ opencv/projects/objectTracking/main.py
@@ -1,22 +1,25 @@
-'''
+"""
 Object Tracking with Opencv and Python
 https://www.youtube.com/watch?v=O3b8lVF93jU
 
-15:15
+python 3.10
 conda install numpy
-'''
-
-
+"""
 import cv2
 from tracker import *
+import getpass
 
-
+BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Videos/Captures/'
+# "modrain.jpg"#"grains.jpg" #
+vid = "1.mp4"
+path = BASE_FOLDER + vid
+#print(path)
 frameWidth = 640
 frameHeight = 480
 # Create tracker object
 tracker = EuclideanDistTracker()
 
-cap = cv2.VideoCapture("highway.mp4")
+cap = cv2.VideoCapture(path)#("highway.mp4")
 
 # Object detection from Stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2 \
@@ -47,9 +50,17 @@ while True:
           x, y, w, h = cv2.boundingRect(cnt)
           detections.append([x, y, w, h])
           #print(detections)
-          cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+          #cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
  
-        # 2. Object Tracking
+    # 2. Object Tracking
+    boxes_ids = tracker.update(detections)
+    #print(boxes_ids)#[[ x,y,width,hight,ID]]
+    for box_id in boxes_ids:
+        x, y, w, h, id = box_id
+        cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+
     cv2.imshow("roi", roi)
     cv2.imshow("Mask", mask)
     img = cv2.resize(frame, (frameWidth, frameHeight))
@@ -59,40 +70,6 @@ while True:
     key = cv2.waitKey(10)
     if key == 27:
         break
-'''
-    # Extract Region of interest
-    roi = frame[340: 720,500: 800]
 
-    # 1. Object Detection
-    mask = object_detector.apply(roi)
-    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    detections = []
-    for cnt in contours:
-        # Calculate area and remove small elements
-        area = cv2.contourArea(cnt)
-        if area > 100:
-            #cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
-            x, y, w, h = cv2.boundingRect(cnt)
-
-
-            detections.append([x, y, w, h])
-
-    # 2. Object Tracking
-    boxes_ids = tracker.update(detections)
-    for box_id in boxes_ids:
-        x, y, w, h, id = box_id
-        cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
-    cv2.imshow("roi", roi)
-  
-    cv2.imshow("Frame", frame)
-    #cv2.imshow("Mask", mask)
-
-    key = cv2.waitKey(30)
-    if key == 27:
-        break
-   '''
 cap.release()
 cv2.destroyAllWindows()
