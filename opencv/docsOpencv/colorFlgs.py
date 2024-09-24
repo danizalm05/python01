af459123e8ba0919548a30a9f26f004181a78290
danizalm05 <danizalm05@gmail.com>
2024-09-24 17:14:51 +0300

    9428

--- opencv/docsOpencv/colorFlgs.py
+++ opencv/docsOpencv/colorFlgs.py
@@ -1,5 +1,5 @@
 """
-                  colorspaces
+                Print Color flags list
 https://docs.opencv.org/4.x/df/d9d/tutorial_py_colorspaces.html
 https://docs.opencv.org/4.x/d2/d96/tutorial_py_table_of_contents_imgproc.html
 https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html 
@@ -8,10 +8,10 @@ https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
 
 import cv2 as cv
 
-
-
-
 flags = [i for i in dir(cv) if i.startswith('COLOR_')]
-print( flags  )
-for p in flags:
-    print (p)
+print( len(flags))
+
+for (i, item) in enumerate(flags, start=1):
+    print('[',i,']', item)
+ 
+print(dir(cv))
