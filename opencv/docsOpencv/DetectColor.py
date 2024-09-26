f1719e9e49595a7e928f6df6631d7ddc31148ec3
Richm <ric5hme45@yahoo.com>
2024-09-26 22:00:58 +0300

    7185

--- opencv/docsOpencv/DetectColor.py
+++ opencv/docsOpencv/DetectColor.py
@@ -1,5 +1,5 @@
 '''
-DetectColors.py
+      DetectColors.py
 
 https://www.youtube.com/watch?v=WQeoO7MI0Bs&t=549s  56:00 1:06:00   1:15:00
 https://github.com/murtazahassan/Learn-OpenCV-in-3-hours/blob/master/chapter7.py'''
@@ -10,7 +10,7 @@ import getpass
 import numpy as np
 import sys
 import os
-from inpOpenCV import stackImages,PutTextOnImage, inpTrackbar
+from inpOpenCV import stackImages,PutTextOnImage, inpTrackbar, get_limits
 
 
 image_name =  'lambo.png' #'lambo.png' 'cards.jpg'
@@ -22,7 +22,9 @@ if not (os.path.isfile(path)):
     print("ERROR --> Missing File: " + path   )
     sys.exit(1)
 
-
+inpWin = "TrackBars"
+scale = 0.2
+inpTrackbar(inpWin)
 
 def empty(a):
     pass
@@ -56,18 +58,18 @@ def stackImages(scale,imgArray):
         hor= np.hstack(imgArray)
         ver = hor
     return ver
-winName = "InpBars"
-cv2.namedWindow(winName)
-#inpTrackbar(winName)
 
-cv2.namedWindow(winName)
-cv2.resizeWindow(winName,640,320)
-cv2.createTrackbar("Hue Min",winName,9,255,empty)
-cv2.createTrackbar("Hue Max",winName,24,179,empty)
-cv2.createTrackbar("Sat Min",winName,30,255,empty)
-cv2.createTrackbar("Sat Max",winName,240,255,empty)
-cv2.createTrackbar("Val Min",winName,175,255,empty)
-cv2.createTrackbar("Val Max",winName,250,255,empty)
+
+'''
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,320)
+cv2.createTrackbar("Hue Min","TrackBars",9,255,empty)
+cv2.createTrackbar("Hue Max","TrackBars",24,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",30,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",175,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",250,255,empty)
+'''
 
 img = cv2.imread(path)
 imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
@@ -75,38 +77,42 @@ imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 colRect = np.ones((212, 212, 3), np.uint8)
 cv2.rectangle(colRect, (1,1), (212, 212), (0, 255, 0), -1)
 while True:
-   
-
+    scale = cv2.getTrackbarPos("Scale", inpWin)/10 
     #        hue
-    h_min = cv2.getTrackbarPos("Hue Min",winName)
-    h_max = cv2.getTrackbarPos("Hue Max",winName)
+    h_min = cv2.getTrackbarPos("Hue Min",inpWin)
+    h_max = cv2.getTrackbarPos("Hue Max", inpWin)
     #        saturation
-    s_min = cv2.getTrackbarPos("Sat Min", winName)
-    s_max = cv2.getTrackbarPos("Sat Max", winName)
+    s_min = cv2.getTrackbarPos("Sat Min", inpWin)
+    s_max = cv2.getTrackbarPos("Sat Max", inpWin)
     #        value
-    v_min = cv2.getTrackbarPos("Val Min", winName)
-    v_max = cv2.getTrackbarPos("Val Max", winName)
-
-    print(h_min,h_max,s_min,s_max,v_min, v_max)
+    v_min = cv2.getTrackbarPos("Val Min", inpWin)
+    v_max = cv2.getTrackbarPos("Val Max", inpWin)
+    
+   
+    
+    #print(h_min,h_max,s_min,s_max,v_min, v_max)
     lower = np.array([h_min,s_min,v_min])#array of 3 min values
     upper = np.array([h_max,s_max,v_max])
-
+  
     mask = cv2.inRange(imgHSV,lower,upper)
     imgResult = cv2.bitwise_and(img,img,mask=mask)
 
     hue =(h_min + h_max)/2
     sat =(s_min +s_max)/2
     val =(v_min + v_max)/2
-
-    print(hue,sat,val)
+     
+    print(scale)
+    
     cv2.rectangle(colRect, (1,1), (212, 212),(hue,sat,val), -1)
-    #cv2.imshow("Image", img)
-    #cv2.imshow("imgHSV", imgHSV)
-    imgStack = stackImages(0.5,([imgHSV,colRect,img],[mask,imgResult,colRect]))
+    
+     
+    imgStack = stackImages(scale,([imgHSV,colRect,img],[mask,imgResult,colRect]))
     #all white values in mask will appear in the result
     cv2.imshow("Result", imgResult)
     cv2.imshow("Stacked Images", imgStack)
-
+     
+     
+    cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 #cap.release()
