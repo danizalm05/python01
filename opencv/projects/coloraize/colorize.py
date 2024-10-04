ed8d26545e4803c6b5dd58ff19fbc7c8d96a9651
Richm <ric5hme45@yahoo.com>
2024-10-04 12:45:26 +0300

    25585

--- opencv/projects/coloraize/colorize.py
+++ opencv/projects/coloraize/colorize.py
@@ -2,6 +2,7 @@
 Black and white image colorization using Python, OpenCV and Deep Learning
 https://www.youtube.com/watch?v=gAmskBNz_Vc
 https://github.com/balajisrinivas/Colorizing-black-and-white-images-using-Python/blob/master/colorize.py
+6:17
 """
 
 # Import statements
@@ -12,17 +13,32 @@ import os
 import getpass
 """
 Download the model files: 
-1. colorization_deploy_v2.prototxt:    https://github.com/richzhang/colorization/tree/caffe/colorization/models
-2. pts_in_hull.npy:					   https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
-3. colorization_release_v2.caffemodel: https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1
+1. colorization_deploy_v2.prototxt:   
+   https://github.com/richzhang/colorization/blob/caffe/colorization/models/colorization_deploy_v2.prototxt
+2. pts_in_hull.npy:					   
+   https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
+3. colorization_release_v2.caffemodel: 
+   https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1
 
 """
 
 # Paths to load the model
 DIR = r"C:\Users\Balaji\Documents\colorize"
 
- 
-BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
+
+BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Resources/colorize/'
 mimg = "pic3.jpg"# 'pic2.jpg'
 img_path  = BASE_FOLDER + mimg
-print(img_path) 
+
+
+# When an 'r'  a character following a backslash is included in the string without change
+# Paths to load the model
+DIR = r"C:\Users\Balaji\Documents\colorize"
+PROTOTXT = os.path.join(DIR, r"model/colorization_deploy_v2.prototxt")
+POINTS = os.path.join(DIR, r"model/pts_in_hull.npy")
+
+MODEL = os.path.join(BASE_FOLDER, "colorization_release_v2.caffemodel")
+
+# Argparser
+ap = argparse.ArgumentParser()
+print(MODEL)
