94e1c2c5b1686116fa6a0045d27d36530c70050e
danizalm05 <danizalm05@gmail.com>
2024-10-07 16:27:05 +0300

    30720

--- opencv/bnsreenu/image_processing_APEER/04-imread.py
+++ opencv/bnsreenu/image_processing_APEER/04-imread.py
@@ -1,42 +1,29 @@
-#!/usr/bin/env python
 '''
-17 - Read and  count only images files in a directory
-Apeer_micro
-
-######################################################################################
-### Reading multiple images from a folder
-#The glob module finds all the path names
-#matching a specified pattern according to the rules used by the Unix shell
-#The glob.glob returns the list of files with their full path
-
-https://www.youtube.com/watch?v=52pMFnkDU-4
-https://github.com/bnsreenu/python_for_microscopists/blob/master/017-Reading_Images_in_Python.py
-
+read image with 'skimage'
+ filters.gaussian
+ 
+ 
+ https://www.youtube.com/watch?v=wLYdpcOxyWA&list=PLHae9ggVvqPgyRQQOtENr6hK0m1UquGaG&index=4
+ https://github.com/bnsreenu/python_for_image_processing_APEER/blob/master/tutorial04_appreciating%20the%20simplicity%20of%20python.py
 '''
 import cv2 as cv
 import glob
 import os
 from PIL import Image
-
+from skimage import io, filters
 from matplotlib import pyplot as plt
+ 
 
 # C:\Users\rockman\Anaconda3\envs\tensorflow
 USER1 = 'gilfm'
 USER2 = 'rockman'
 IMAGE_NAME = '1.jpg'
-BASE_FOLDER = 'C:/Users/' + USER1 + '/Pictures/Saved Pictures/'
+BASE_FOLDER = 'C:/Users/' + USER2 + '/Pictures/Saved Pictures/'
 IMAGE = BASE_FOLDER + IMAGE_NAME
 # Video Playlist: https://www.youtube.com/playlist?list=PLHae9ggVvqPgyRQQOtENr6hK0m1UquGaG
-print(IMAGE)
-
-"""
-Introductory python tutorials for image processing
-
-Tutorial 3: Appreciating the simplicity of Python code
-
-"""
 
 img = io.imread(IMAGE)
 gaussian_img = filters.gaussian(img, sigma=1)
-
 plt.imshow(gaussian_img)
+
+print("image = ",IMAGE)
