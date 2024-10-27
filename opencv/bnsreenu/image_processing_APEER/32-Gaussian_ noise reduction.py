d056ba9c5323aa55d3b3f5bcf84adaec5d29ae37
danizalm05 <danizalm05@gmail.com>
2024-10-27 16:13:50 +0200

    13937

--- opencv/bnsreenu/image_processing_APEER/32-Gaussian_ noise reduction.py
+++ opencv/bnsreenu/image_processing_APEER/32-Gaussian_ noise reduction.py
@@ -14,10 +14,14 @@ skimage.filters.gaussian - https://scikit-image.org/docs/dev/api/skimage.filters
 import cv2
 import getpass
 import os
+import cv2
+from skimage import io, img_as_float
+from skimage.filters import gaussian
+
 
 USER = getpass.getuser()
 
-IMAGE_NAME = 'lena.jpg' #'2.jpg'
+IMAGE_NAME = '2.jpg' #'2.jpg' 'lena.jpg'
 BASE_FOLDER = 'C:/Users/' + USER + '/Pictures/Saved Pictures/'
 IMAGE = BASE_FOLDER + IMAGE_NAME
 
@@ -28,7 +32,26 @@ file_list = os.listdir(path)
 for (image) in os.listdir(path):  # iterate through each file to perform some action
     print(image)
 
- 
+img_gaussian_noise = img_as_float(io.imread(IMAGE, as_gray=True))
+img_salt_pepper_noise = img_as_float(io.imread(IMAGE, as_gray=True))
+
+img = img_gaussian_noise
+
+gaussian_using_cv2 = cv2.GaussianBlur(img, (3,3), 0, borderType=cv2.BORDER_CONSTANT)
+
+gaussian_using_skimage = gaussian(img, sigma=1, mode='constant', cval=0.0)
+#sigma defines the std dev of the gaussian kernel. SLightly different than
+#how we define in cv2
+
+
+cv2.imshow("Original", img)
+cv2.imshow("Using cv2 gaussian", gaussian_using_cv2)
+cv2.imshow("Using skimage", gaussian_using_skimage)
+#cv2.imshow("Using scipy2", conv_using_scipy2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 
 
