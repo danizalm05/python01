1fdafe608cfa01c312b69007dfe6aed3f0034748
danizalm05 <danizalm05@gmail.com>
2024-11-04 15:38:10 +0200

    3125

--- opencv/bnsreenu/image_processing_APEER/42-Histogram equalization .py
+++ opencv/bnsreenu/image_processing_APEER/42-Histogram equalization .py
@@ -5,10 +5,7 @@ https://github.com/bnsreenu/python_for_image_processing_APEER/blob/master/tutori
 https://www.youtube.com/watch?v=48WvE9znnRw&list=PLHae9ggVvqPgyRQQOtENr6hK0m1UquGaG&index=43
 5:23
 
-"""
-
-"""
-            CLAHE: COntrast limited adaptive histogram equalization
+CLAHE: COntrast limited adaptive histogram equalization
 --------------------------------------------------------------
 Regular histogram equalization uses global contrast of the image. 
 This results in too bright and too dark regions as the histogram stretches 
@@ -42,7 +39,7 @@ import sys
 
 USER = getpass.getuser()
 
-IMAGE_NAME = 'lena.jpg' #'2.jpg'
+IMAGE_NAME = '22.jpg' #'2.jpg' 'lena.jpg'
 BASE_FOLDER = 'C:/Users/' + USER + '/Pictures/Saved Pictures/'
 IMAGE = BASE_FOLDER + IMAGE_NAME
 
@@ -62,7 +59,42 @@ lab_img= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 #Splitting the LAB image to L, A and B channels, respectively
 l, a, b = cv2.split(lab_img)
 
+
+
+###########Histogram Equlization#############
+#Apply histogram equalization to the L channel
+equ = cv2.equalizeHist(l)
+
 plt.hist(l.flat, bins=100, range=(0,255))
+plt.hist(equ.flat, bins=100, range=(0,255))
 plt.show()
 
+#Combine the Hist. equalized L-channel back with A and B channels
+updated_lab_img1 = cv2.merge((equ,a,b))
+
+#Convert LAB image back to color (RGB)
+hist_eq_img = cv2.cvtColor(updated_lab_img1, cv2.COLOR_LAB2BGR)
+
+
+#seconed method
+###########CLAHE#########################
+#Apply CLAHE to L channel
+clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+clahe_img = clahe.apply(l)
+#plt.hist(clahe_img.flat, bins=100, range=(0,255))
+
+#Combine the CLAHE enhanced L-channel back with A and B channels
+updated_lab_img2 = cv2.merge((clahe_img,a,b))
+
+#Convert LAB image back to color (RGB)
+CLAHE_img = cv2.cvtColor(updated_lab_img2, cv2.COLOR_LAB2BGR)
+
+
+
+cv2.imshow("Original image", img)
+cv2.imshow("equ", equ)
+cv2.imshow("Equalized image", hist_eq_img)
+cv2.imshow('CLAHE Image', CLAHE_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
