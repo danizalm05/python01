c9dc7b64016be9e8e04372b7573d8b0262098add
danizalm05 <danizalm05@gmail.com>
2024-11-05 15:42:53 +0200

    24051

--- PyQt/pyqt6/freeCodeCamp/1.EnvironmentSetup/main.py
+++ PyQt/pyqt6/freeCodeCamp/1.EnvironmentSetup/main.py
@@ -3,8 +3,8 @@
 
  https://github.com/rutura/Qt-For-Python-PySide6-GUI-For-Beginners-The-Fundamentals-
  https://www.youtube.com/watch?time_continue=3&v=Z1N9JzNax2k&embeds_referring_euri=https%3A%2F%2Fduckduckgo.com%2F
-https://doc.qt.io/qtforpython-6/
-52:00
+     https://doc.qt.io/qtforpython-6/
+56:00
 '''
 
 import sys
