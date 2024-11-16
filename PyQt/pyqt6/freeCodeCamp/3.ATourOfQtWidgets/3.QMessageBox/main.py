b77268fed88289e4eec1accf3b8b8fa6f59cb2b9
danizalm05 <danizalm05@gmail.com>
2024-11-16 15:53:53 +0200

    16846

---
+++ PyQt/pyqt6/freeCodeCamp/3.ATourOfQtWidgets/3.QMessageBox/main.py
@@ -0,0 +1,21 @@
+'''
+ Learn Python GUI Development for Desktop – PySide6 and Qt Tutorial
+
+
+https://www.youtube.com/watch?v=Z1N9JzNax2k&t=3s
+https://github.com/rutura/Qt-For-Python-PySide6-GUI-For-Beginners-The-Fundamentals-/tree/main/3.ATourOfQtWidgets/3.QMessageBox
+01:45: -   1: :00
+
+'''
+from PySide6.QtWidgets import QApplication
+from widget import Widget
+import sys
+
+app = QApplication(sys.argv)
+
+widget = Widget()
+widget.show()
+
+app.exec()
+
+QApplication.shutdown(app) 
---
+++ opencv/bnsreenu/image_processing_APEER/46  image registration libraries.py
@@ -0,0 +1,33 @@
+"""
+46 - Useful image registration libraries in python 
+#-----------------------------------------------------
+https://www.youtube.com/watch?v=F4AV6SsUxXE&list=PLHae9ggVvqPgyRQQOtENr6hK0m1UquGaG&index=47
+https://github.com/bnsreenu/python_for_image_processing_APEER/blob/master/tutorial46_img_registration_libraries_in_python.py
+ 
+Learning from the astronomy guys...
+A couple of ways to perform image registration
+https://image-registration.readthedocs.io/en/latest/image_registration.html
+
+pip install image_registration
+ 
+
+"""
+import cv2
+
+import getpass
+import os
+
+USER = getpass.getuser()
+
+BASE_FOLDER = 'C:/Users/' + USER + '/Pictures/Saved Pictures/'
+IMAGE_NAME = '2.jpg'  # '2.jpg' 'lena.jpg'
+
+img_list = []
+
+for (image) in os.listdir(BASE_FOLDER):  # iterate through each file to perform some action
+    img_list.append(image)
+
+img_num = 17
+IMAGE = BASE_FOLDER + img_list[img_num]
+# IMAGE = BASE_FOLDER + IMAGE_NAME
+print(IMAGE)
