74b19cdbcde040158c80fb338530931f96b9fae2
danizalm05 <danizalm05@gmail.com>
2025-01-01 16:29:24 +0200

    26395

--- opencv/bnsreenu/image_processing_APEER/55segmentation_followed_by_measurements.py
+++ opencv/bnsreenu/image_processing_APEER/55segmentation_followed_by_measurements.py
@@ -11,7 +11,7 @@ image:
 https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.regionprops
 https://github.com/scikit-image/scikit-image/blob/v0.17.2/skimage/measure/_regionprops.py#L643
  
-03:30
+10:30
 
 """
 
@@ -39,9 +39,38 @@ image = cv2.imread(IMAGE1, 0)#Gray image
 plt.imshow(image, cmap='gray')
 scale = 0.6 #microns/pixel
 
-plt.hist(image.flat, bins=100, range=(0,150))  #.flat returns the flattened numpy array (1D)
+#plt.hist(image.flat, bins=100, range=(0,170))  #.flat returns the flattened numpy array (1D)
+
+from skimage.filters import threshold_otsu
+threshold = threshold_otsu(image)
+
+#Generate thresholded image
+thresholded_img = image < threshold
+plt.imshow(thresholded_img)
 
 
+#Remove edge touching regions
+from skimage.segmentation import clear_border
+edge_touching_removed = clear_border(thresholded_img)
+plt.imshow(edge_touching_removed)
+
+
+#Label connected regions of an integer array using measure.label
+#Labels each connected entity as one object
+#Connectivity = Maximum number of orthogonal hops to consider a pixel/voxel as a neighbor. 
+#If None, a full connectivity of input.ndim is used, number of dimensions of the image
+#For 2D image it would be 2
+
+label_image = measure.label(edge_touching_removed, connectivity=image.ndim)
+
+plt.imshow(label_image)
+#Return an RGB image where color-coded labels are painted over the image.
+#Using label2rgb
+plt.show()
+
+image_label_overlay = label2rgb(label_image, image=image)
+plt.imshow(image_label_overlay) 
+plt.show()
 img = image
 
 
@@ -62,29 +91,27 @@ ax1.imshow(img, cmap='gray')
 ax1.title.set_text('Input Image')
 
 ax2 = fig.add_subplot(3,3,2)
-ax2.imshow(img , cmap='gray')
-ax2.title.set_text(' entropy_img')
+ax2.imshow(thresholded_img, cmap='gray')
+ax2.title.set_text('thresholded_img')
 
 ax3 = fig.add_subplot(3,3,3)
-
 ax3.hist(img.flat, bins=100, range=(0,255))
 ax3.title.set_text('hist color range 100,255')
 
 ax4 = fig.add_subplot(3,3,4)
-ax4.imshow(img, cmap='gray')
-ax4.title.set_text('binary')
+ax4.imshow(edge_touching_removed, cmap='gray')
+ax4.title.set_text('edge_touching_removed')
 
 
 ax5 = fig.add_subplot(3,3,5)
-ax5.imshow(img,cmap='gray')
-ax5.title.set_text('Entropy')
+ax5.imshow(label_image)#,cmap='gray')
+ax5.title.set_text('label_image')
 
 
 
 ax6 = fig.add_subplot(3,3,6)
-plt.hist(img.flat, bins=100, range=(0,5))
-#ax6.imshow(img, cmap='gray')
-ax6.title.set_text('entropy hist')
+ax6.imshow(image_label_overlay)#, cmap='gray')
+ax6.title.set_text('image_label_overlay')
 
 ax7 = fig.add_subplot(3,3,7)
 ax7.imshow(img, cmap='gray')
