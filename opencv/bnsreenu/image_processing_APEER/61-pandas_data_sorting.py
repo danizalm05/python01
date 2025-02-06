eb8d889be62e9657103fdb6286926181580ce86d
danizalm05 <danizalm05@gmail.com>
2025-02-06 17:01:26 +0200

    31852

--- opencv/bnsreenu/image_processing_APEER/61-pandas_data_sorting.py
+++ opencv/bnsreenu/image_processing_APEER/61-pandas_data_sorting.py
@@ -15,3 +15,22 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data/manual_vs_auto_updated.csv')
 print(df.head())
 print("\n\n\ndf.columns = ",df.columns)
+
+#Let us sort and assign this to a diferent variable
+df2=df.sort_values('Manual', ascending=True)
+#We can select just a subset of data, for example to only get Manual column
+print(df2['Manual'])
+
+#To get multiple columns, it is just
+
+title_list = ['Manual', 'Auto_th_2']
+print(   df[ title_list ] )
+
+#To select subset of rows
+print(df[20:30])  #Extracts rows 20 to 30, not including 30.
+
+#Combining above two, to get specific columns from specific rows.
+rows =  list(range(20,30))
+colums = ['Manual', 'Auto_th_2']
+#04:26
+print(df.loc[rows,colums]) #    print(df.loc[20:30, ['Manual', 'Auto_th_2']])
