ba1b7bbe8b0ec6d97d9e7c3bf00831616d27ec78
danizalm05 <danizalm05@gmail.com>
2025-02-18 16:24:27 +0200

    2483

--- opencv/bnsreenu/image_processing_APEER/65_pandas_data_plotting_using_Seaborn.py
+++ opencv/bnsreenu/image_processing_APEER/65_pandas_data_plotting_using_Seaborn.py
@@ -14,7 +14,7 @@ the box environment.
 https://seaborn.pydata.org/examples/index.html   
  pip install seaborn
  
- 7:00
+ 12:00
 """    
  
 import pandas as pd
@@ -47,19 +47,58 @@ df['Manual'].fillna(100, inplace=True)
 #============================   Output  ===============================   
 
 fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(12, 8))
+fig.tight_layout(pad=3.0)
  #The overlay over histogram is KDE plot (Kernel density distribution)
+fig.suptitle("This Main Title is Nicely Formatted", fontsize=16)
+
+axes[0, 0].set_title(' [0,0] Subtitle')
 sns.distplot(df['Manual'], ax=axes[0, 0])
-#Making it visually appealing
-sns.distplot(df['Manual'], bins=20, kde=True, rug=False, hist_kws=dict(edgecolor='k', linewidth=0.8),ax=axes[0, 1]) 
 
 
+axes[0, 1].set_title('[0,1] Subtitle')
+sns.distplot(df['Manual'], bins=20, kde=True, rug=True, hist_kws=dict(edgecolor='k', linewidth=0.8),ax=axes[0, 1]) 
+
+axes[0, 2].set_title('kdeplot')
+sns.kdeplot(df['Manual'], shade=True,ax=axes[0, 2])
+
+## Add Multiple plots
+axes[1, 0].set_title('Auto_th_2 3 4')
+sns.kdeplot(df['Auto_th_2'], shade=True,ax=axes[1, 0])
+sns.kdeplot(df['Auto_th_3'], shade=True,ax=axes[1, 0])
+sns.kdeplot(df['Auto_th_4'], shade=True,ax=axes[1, 0])
+
+axes[1,1].set_title('Simple line')
+sns.set(style='darkgrid')   #Adds a grid
+sns.lineplot(x='Image', y='Manual', data=df, hue='Unnamed: 0',ax=axes[1, 1])  
+ #Simple line plot
+#Hue tells seaborn how to color various subcategories, like our set in this example.
+
+
+
+
+
+
+ 
 plt.show()
 
  
 
 
+sns.distplot(df['Manual'], bins=20, kde=True, rug=False, hist_kws=dict(edgecolor='y', linewidth=0.8)) 
 plt.xlim([80,120])# show values from 80 to 120
-sns.distplot(df['Manual'], bins=20, kde=True, rug=False, hist_kws=dict(edgecolor='y', linewidth=0.8),ax=axes[0, 2]) 
+plt.show()
+sns.kdeplot(df['Manual'], shade=True)
+
+## Add Multiple plots
+sns.kdeplot(df['Auto_th_2'], shade=True)
+sns.kdeplot(df['Auto_th_3'], shade=True)
+sns.kdeplot(df['Auto_th_4'], shade=True)
+
+plt.show()
+
+
+sns.lineplot(x='Image', y='Manual', data=df, hue='Unnamed: 0')
+
 plt.show()
 '''
 
