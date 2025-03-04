f1e3fc7699bc6e36fb1e6e3c9a0bfd8909c3d539
danizalm05 <danizalm05@gmail.com>
2025-03-04 17:08:39 +0200

    29032

--- opencv/bnsreenu/image_processing_APEER/68-linear_regressio.py
+++ opencv/bnsreenu/image_processing_APEER/68-linear_regressio.py
@@ -59,3 +59,35 @@ X_train, X_test, y_train, y_test = \
 #By doing this we work with same test dataset evry time, 
 #if this is important.
 #random_state=None splits dataset randomly every time
+
+#-----------------------------------------------------------
+#       Y =   the value we want to predict
+#       X = all independent variables upon which Y depends. 
+#  steps for linear regression....
+# Step 1:   Create the instance of the model
+# Step 2:   .fit() to train the model or fit a linear model
+# Step 3:   .predict() to predict Y for given X values. 
+# Step 4:   Calculate the accuracy of the model. 
+
+#TO create a model instance 
+from sklearn import linear_model
+model = linear_model.LinearRegression()  #Create an instance of the model.
+model.fit(X_train, y_train)   #Train the model or fits a linear model
+
+print("model.score(X_train, y_train) = ",model.score(X_train, y_train))  #Prints the R^2 value, a measure of how well
+#observed values are replicated by themodel. 
+
+
+prediction_test = model.predict(X_test)    
+print("\ny_test \n ------- \n",y_test )
+print("\nprediction_tes  = ", prediction_test)
+
+print("\nMean sq. errror between y_test and predicted =", np.mean(prediction_test-y_test)**2)
+# A MSE value of about 8 is not bad compared to average # cells about 250.
+
+
+#Residual plot
+plt.scatter(prediction_test, prediction_test-y_test)
+plt.hlines(y=0, xmin=200, xmax=300)# just draw the y=0 axis
+plt.show()
+#Plot would be useful for lot of data points
