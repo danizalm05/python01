dd504c50212981111fe848f9a0023fec06cbae62
Richm <ric5hme45@yahoo.com>
2025-03-05 22:22:31 +0200

    12227

---
+++ opencv/bnsreenu/image_processing_APEER/69-multi_linear_regression.py
@@ -0,0 +1,42 @@
+"""
+69-multi_linear_regression.py
+ -------------------------------------------https://www.youtube.com/watch?v=9CxJhQynU20&list=PLHae9ggVvqPgyRQQOtENr6hK0m1UquGaG&index=70
+https://www.youtube.com/watch?v=_gTr5DtndeU&list=PLHae9ggVvqPgyRQQOtENr6hK0m1UquGaG&index=71
+https://github.com/bnsreenu/python_for_image_processing_APEER/blob/master/tutorial69-multi_linear_regression.py
+ 
+Multiple Linear Regression uses several explanatory 
+variables to predict the outcome of a response variable.
+
+There are a lot of variables and multiple linear 
+regression is designed to create a model 
+based on all these variables. 
+
+#Dataset link:
+https://cdn.scribbr.com/wp-content/uploads//2020/02/heart.data_.zip?_ga=2.217642335.893016210.1598387608-409916526.1598387608
+
+#Heart disease
+The effect that the independent variables biking and 
+smoking  have on the dependent variable heart disease 
+
+"""
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+df = pd.read_csv('data/heart_data.csv')
+#print(df.head())
+
+df = df.drop("Unnamed: 0", axis=1)#Drop the first column
+print(df.head())
+
+#A few plots in Seaborn to understand the data 
+sns.lmplot(x='biking', y='heart.disease', data=df)  
+sns.lmplot(x='smoking', y='heart.disease', data=df)  
+
+
+x_df = df.drop('heart.disease', axis=1)
+y_df = df['heart.disease']
+
+
+ 
