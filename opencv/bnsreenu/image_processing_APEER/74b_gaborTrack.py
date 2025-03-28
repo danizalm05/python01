7b589f1ded2aa9bf3dab4cdde41a8e1fccf12dd3
Richm <ric5hme45@yahoo.com>
2025-03-28 17:04:08 +0300

    4923

--- opencv/bnsreenu/image_processing_APEER/74b_gaborTrack.py
+++ opencv/bnsreenu/image_processing_APEER/74b_gaborTrack.py
@@ -49,9 +49,49 @@ IMAGE = BASE_FOLDER + IMAGE_NAME
 if not(Path(IMAGE).exists()):
     msg = "Error: file " + IMAGE +" does not exist"
     sys.exit(msg)
-   
+
+
+#Display Group of images in one window
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+    
+
+
+
+
+
+
+
 img = cv2.imread(IMAGE)#Gray image
-plt.imshow(img)
+#plt.imshow(img)
 
 ksize = 5  #Use size that makes sense to the image and fetaure 
 #          size. Large may not be good. 
@@ -64,6 +104,39 @@ gamma=0.5  #Value of 1 defines spherical. Calue close to 0 has
 #Value of 1, spherical may not be ideal as it picks up features from other regions.
 phi = 1.0  #Phase offset. I leave it to 0. (For hidden pic use 0.8)
 
+def nothing(x):
+    pass
+ 
+wname = 'image'
+cv2.namedWindow(wname)
+ 
+# create trackbars for color change
+cv2.createTrackbar('scale',wname,0,10,nothing)
+ 
+cv2.createTrackbar('G',wname,0,255,nothing)
+cv2.createTrackbar('B',wname,0,255,nothing)
+ 
+# create switch for ON/OFF functionality
+switch = '0 : OFF \n1 : ON'
+cv2.createTrackbar(switch, wname,0,1,nothing)
+
+
+
+
+     
+while True:
+   
+    scale = 0.4 + cv2.getTrackbarPos("scale", wname) / 10
+    print(scale)
+    imgStack = stackImages( scale,  ([img,img ])    )
+    cv2.imshow("img", imgStack)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+'''
+
 
 kernel = cv2.getGaborKernel((ksize, ksize), sigma, theta, lamda, gamma, phi, ktype=cv2.CV_32F)
 
@@ -74,80 +147,7 @@ fimg = cv2.filter2D(img, cv2.CV_8UC3, kernel)
 
 kernel_resized = cv2.resize(kernel, (400, 400))                    # Resize image
 
-
-
-
-#plt.imshow(kernel)
-
-
-
-
-
-#============================   Output  ===============================   
-
-fig = plt.figure(figsize=(16, 16))
-plt.subplots_adjust ( hspace=0.6)
-
-ax1 = fig.add_subplot(4,3,1)
-ax1.imshow(img, cmap='gray')
-ax1.title.set_text('Input Image')
-
-ax2 = fig.add_subplot(4,3,2)
-ax2.imshow(kernel)#, cmap='gray')
-ax2.title.set_text('kernel')
-
-
-ax3 = fig.add_subplot(4,3,3)
-ax3.hist(img.flat, bins=100, range=(0,255))
-ax3.title.set_text('hist color range')
- 
-ax4 = fig.add_subplot(4,3,4)
-ax4.imshow(img_g, cmap='gray')
-ax4.title.set_text('Gray color')
-
-
-ax5 = fig.add_subplot(4,3,5)
-ax5.imshow(kernel_resized)#,cmap='gray')
-ax5.title.set_text('kernel_resized')
-
-
-
-ax6 = fig.add_subplot(4,3,6)
-ax6.imshow(fimg )#, cmap='gray')
-ax6.title.set_text('result')
 '''
-ax7 = fig.add_subplot(4,3,7)
-ax7.imshow(sure_bg, cmap='gray')
-ax7.title.set_text('surebackground')
-
- 
-
-ax8 = fig.add_subplot(4,3,8)
-ax8.imshow( sure_fg , cmap='gray')
-ax8.title.set_text(' sure_fg ')
 
 
-ax9 = fig.add_subplot(4,3,9)
-ax9.imshow(unknown, cmap='gray')
-ax9.title.set_text('unknown')
-
-#
-ax10 = fig.add_subplot(4,3,10)
-ax10.imshow(markers)#, cmap='gray')
-ax10.title.set_text('markers')
-
- 
-
-ax11 = fig.add_subplot(4,3,11)
-ax11.imshow(markers10,cmap='gray')
-ax11.title.set_text('markers10')
-
- 
-
-ax12 = fig.add_subplot(4,3,12)
-ax12.imshow(img2,cmap='gray')
-ax12.title.set_text('labels are painted over the image.')
-
-'''
-plt.show()
  
