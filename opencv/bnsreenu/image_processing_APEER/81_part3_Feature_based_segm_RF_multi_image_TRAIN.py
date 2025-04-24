f85e733a755e93f858079a19c4715d18bf801d54
danizalm05 <danizalm05@gmail.com>
2025-04-24 15:19:24 +0300

    163

---
+++ opencv/bnsreenu/image_processing_APEER/81_part3_Feature_based_segm_RF_multi_image_TRAIN.py
@@ -0,0 +1,28 @@
+"""
+80_part2_predict_ML_segmentation_All_filters_RForest
+ -----------------------------------------------------
+
+https://www.youtube.com/watch?v=VCrLaEHShDo&list=PLHae9ggVvqPgyRQQOtENr6hK0m1UquGaG&index=83
+https://github.com/bnsreenu/python_for_image_processing_APEER/blob/master/tutorial81_part3_Feature_based_segm_RF_multi_image_TRAIN.pyimage url
+ 
+
+
+Feature based segmentation using Random Forest
+Demonstration using multiple training images
+
+STEP 1: READ TRAINING IMAGES AND EXTRACT FEATURES 
+
+STEP 2: READ LABELED IMAGES (MASKS) AND CREATE ANOTHER DATAFRAME
+
+STEP 3: GET DATA READY FOR RANDOM FOREST (or other classifier)
+
+STEP 4: DEFINE THE CLASSIFIER AND FIT THE MODEL USING TRAINING DATA
+
+STEP 5: CHECK ACCURACY OF THE MODEL
+
+STEP 6: SAVE MODEL FOR FUTURE USE
+
+STEP 7: MAKE PREDICTION ON NEW IMAGES
+"""
+
+
