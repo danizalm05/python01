7961d240a3d5d43d1e71617c5eca27375f2d6bfa
danizalm05 <danizalm05@gmail.com>
2025-05-02 16:53:21 +0300

    14747

--- opencv/bnsreenu/image_processing_APEER/83_feature_extraction_RF_classification_V2.0.py
+++ opencv/bnsreenu/image_processing_APEER/83_feature_extraction_RF_classification_V2.0.py
@@ -28,9 +28,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import glob
 
-
 import seaborn as sns
-import pandas as pd
 from skimage.filters import sobel
 
 
@@ -126,31 +124,61 @@ x_train, x_test = x_train / max_color, x_test / max_color
 # input shape is (n, x, y, c) - number of images, x, y, and channels
 def feature_extractor(dataset):
     x_train = dataset
-    image_dataset = pd.DataFrame()
-    print(image_dataset)
+    image_dataset = pd.DataFrame()#Create df for each image
+  
     for image in range(x_train.shape[0]):  #iterate through each file 
        print(image, "\n-----\n")
        df = pd.DataFrame()  #Temporary data frame to capture information for each loop.
        #Reset dataframe to blank after each loop.
        
-       input_img = x_train[image, :,:,:]
+       input_img = x_train[image, :,:,:]# image is  the number of the image file
        img = input_img
-       #print(img)
-       
-       
+             
        #START ADDING DATA TO THE DATAFRAME
-   #Add feature extractors, e.g. edge detection, smoothing, etc. 
+       #Add feature extractors, e.g. edge detection, smoothing, etc. 
            
         # FEATURE 1 - Pixel values
-        
-       #Add pixel values to the data frame
-       pixel_values = img.reshape(-1)
+        #Add pixel values to the data frame
+       pixel_values = img.reshape(-1)#Reshape image to a vector
        df['Pixel_Value'] = pixel_values   #Pixel value itself as a feature
        #df['Image_Name'] = image   #Capture image name as we read multiple images
        
-       # FEATURE 2 - Bunch of Gabor filter responses 20:00
+       ######### FEATURE 2 - Bunch of Gabor filter responses 20:00
+       num = 1         #To count numbers up in order to give Gabor features 
+                       #a lable in the data frame
+       kernels = [] 
+       for theta in range(2):   #Define number of thetas
+           theta = theta / 4. * np.pi               
+          
+           for sigma in (1, 3):  #Sigma with 1 and 3
+               lamda = np.pi/4
+               gamma = 0.5
+               gabor_label = 'Gabor' + str(num)  #Label Gabor columns as Gabor1, Gabor2, etc.
+               ksize=9
+               kernel = cv2.getGaborKernel((ksize, ksize), sigma, theta, lamda, gamma, 0, ktype=cv2.CV_32F)    
+               kernels.append(kernel)
+               #Now filter the image and add values to a new column 
+               fimg = cv2.filter2D(img, cv2.CV_8UC3, kernel)
+               filtered_img = fimg.reshape(-1)
+               df[gabor_label] = filtered_img  #Labels columns as Gabor1, Gabor2, etc.
+               print(gabor_label, ': theta=', theta, ': sigma=', sigma, ': lamda=', lamda, ': gamma=', gamma)
+               num += 1  #Increment for gabor column label
+               
+             # FEATURE 3 Sobel
+       '''
+       edge_sobel = sobel(img)   
+       edge_sobel1 = edge_sobel.reshape(-1)
+       df['Sobel'] = edge_sobel1
+       '''
+        #Add more filters as needed        
+     #Append features from current image to the dataset
+       #image_dataset = image_dataset.append(df) #this command  is an error
+       image_dataset = pd.concat([image_dataset, df])#, ignore_index=True)
+      
+    return image_dataset
+
 ####################################################################
-#Extract features from training images
+#21:00
 
 
 
