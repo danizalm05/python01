31dd7d706db7c1be79cff2ea2a30b5b0c0bfb854
danizalm05 <danizalm05@gmail.com>
2025-09-29 16:46:54 +0300

    31905

--- Solidity/src/web3_simple_storage/deploy.py
+++ Solidity/src/web3_simple_storage/deploy.py
@@ -2,17 +2,16 @@
 # https://www.youtube.com/watch?v=M576WGiDBdQ&t=44s   03:30:00
 
 
-
 # you must run  pip install py-solc-x
 # In the video, we forget to `install_solc`
-# change 'from solcx import compile_standard' to 
-from solcx import compile_standard, install_solc 
+# change 'from solcx import compile_standard' to
+from solcx import compile_standard, install_solc
 import json
 
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    #print(simple_storage_file)
+    # print(simple_storage_file)
 # We add these two lines that we forgot from the video!
 print("Installing...")
 install_solc("0.8.7")
@@ -29,11 +28,22 @@ compiled_sol = compile_standard(
             }
         },
     },
-    solc_version= "0.8.7"   #"0.6.0",
-)    
-#print(compiled_sol)
-#03:46:30
+    solc_version="0.8.7",  # "0.6.0",
+)
+# print(compiled_sol)
+# 03:46:30
 
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
- 
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+#         ============   Deploy the contract ================== 03:50:00
