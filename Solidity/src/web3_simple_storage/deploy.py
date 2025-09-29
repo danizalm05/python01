241b604df1167ca173c56b4cbf12490f9bda96d0
Richm <ric5hme45@yahoo.com>
2025-09-29 09:51:17 +0300

    16011

--- Solidity/src/web3_simple_storage/deploy.py
+++ Solidity/src/web3_simple_storage/deploy.py
@@ -1,9 +1,13 @@
 # https://github.com/PatrickAlphaC/web3_py_simple_storage/blob/main/deploy.py
+# https://www.youtube.com/watch?v=M576WGiDBdQ&t=44s   03:30:00
+
+
+
 # you must run  pip install py-solc-x
 # In the video, we forget to `install_solc`
 # change 'from solcx import compile_standard' to 
 from solcx import compile_standard, install_solc 
-
+import json
 
 
 with open("./SimpleStorage.sol", "r") as file:
@@ -25,5 +29,11 @@ compiled_sol = compile_standard(
             }
         },
     },
-    solc_version="0.6.0",
+    solc_version= "0.8.7"   #"0.6.0",
 )    
+#print(compiled_sol)
+#03:46:30
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+ 
