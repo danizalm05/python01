8852f1e80ada10d19d2ce487a5e589a850e5fea8
danizalm05 <danizalm05@gmail.com>
2025-10-01 16:50:54 +0300

    7157

--- Solidity/src/wb3_simple_storage/deploy.py
+++ Solidity/src/wb3_simple_storage/deploy.py
@@ -1,12 +1,16 @@
 # https://github.com/PatrickAlphaC/web3_py_simple_storage/blob/main/deploy.py
 # https://www.youtube.com/watch?v=M576WGiDBdQ&t=44s   03:30:00
+#  
 
-
-# you must run  pip install py-solc-x
-# In the video, we forget to `install_solc`
-# change 'from solcx import compile_standard' to
-from solcx import compile_standard, install_solc
+from web3 import Web3
 import json
+
+
+# First you must run: 
+#       pip install py-solc-x   
+#       pip install web3
+
+from solcx import compile_standard, install_solc
 
 
 with open("./SimpleStorage.sol", "r") as file:
@@ -45,5 +49,32 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = json.loads(
     compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
 )["output"]["abi"]
-print(abi)
-#         ============   Deploy the contract ================== 03:50:00
+#print(abi)
+
+
+#         ============   Deploy the contract ================== 03:53:00
+# For connecting to ganache
+# The next values  are taken from Ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+# the chain id for gaunch is 1337 . the network is 5777
+chain_id = 1337     
+my_address =  "0x9464257eB4a5904A5D9D7d687f12C0fdFf0E237c"
+private_key = "0x588b57422ace77ce5255cfa40f0a4df3520d5b6bca1bce0e2a6cdad45d8301bc"
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.get_transaction_count(my_address)
+print ("nonce = ",nonce) # number of transaction
+
+# Submit the transaction that deploys the contract
+transaction = SimpleStorage.constructor().build_transaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+print ("transaction \n= ",transaction)
+#043:01:00 
