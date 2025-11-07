105b09fd6992176866abd18d44ee79ee98390680
danizalm05 <danizalm05@gmail.com>
2025-11-07 15:35:55 +0200

    23398

--- Solidity/src/wb3_simple_storage/SimpleStorage.sol
+++ Solidity/src/wb3_simple_storage/SimpleStorage.sol
@@ -1,33 +1,42 @@
 // github.com/Cyfrin/remix-storage-factory-cu/blob/main/SimpleStorage.sol
 // SPDX-License-Identifier: MIT
+//https://www.youtube.com/watch?v=umepbfKp5rI&t=10509s
 
-pragma solidity ^0.8.7;
+pragma solidity 0.8.30;
+
 // pragma solidity ^0.8.0;
 // pragma solidity >=0.8.0 <0.9.0;
 
+ 
+
 contract SimpleStorage {
+    uint256 myFavoriteNumber;
 
-    uint256 favoriteNumber;
-
-    struct People {
+    struct Person {
         uint256 favoriteNumber;
         string name;
     }
     // uint256[] public anArray;
-    People[] public people;
+    Person[] public listOfPeople;
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public virtual {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
-    
-    function retrieve() public view returns (uint256){
-        return favoriteNumber;
+
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
     }
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
+        listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
+
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
+
+contract SimpleStorage4 {}
