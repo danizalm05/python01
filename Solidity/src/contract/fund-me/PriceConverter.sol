84447e74456e145179fd45ecd2b88406e45f5976
danizalm05 <danizalm05@gmail.com>
2025-12-19 19:21:31 +0200

    22954

--- Solidity/src/contract/fund-me/PriceConverter.sol
+++ Solidity/src/contract/fund-me/PriceConverter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
+//https://youtu.be/umepbfKp5rI?t=18189
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
 
 // Why is this a library and not abstract?
