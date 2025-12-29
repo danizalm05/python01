c6826e92e8bfb109232c24c8de64accde028eba0
danizalm05 <danizalm05@gmail.com>
2025-12-29 16:47:16 +0200

    28915

--- Solidity/src/contract/fund-me/FundMe.sol
+++ Solidity/src/contract/fund-me/FundMe.sol
@@ -1,90 +1,96 @@
 //SPDX-License-Identifier: MIT
-
-https://github.com/Cyfrin/foundry-full-course-cu?tab=readme-ov-file#solidity-101-section-3-remix-fund-me
-
-https://updraft.cyfrin.io/courses/solidity/fund-me/ 
 //https://github.com/Cyfrin/remix-fund-me-cu/blob/main/FundMe.sol
-
-//https://docs.chain.link/data-feeds 
-//https://docs.chain.link/data-feeds/price-feeds/addresses
-//https://docs.chain.link/data-feeds/price-feeds/addresses?networkType=testnet
-//https://docs.chain.link/data-feeds/price-feeds/addresses?page=7  eth /usd
-//https://docs.chain.link/data-feeds/price-feeds/addresses?networkType=testnet&testnetSearch=eth+%2Fusd&testnetPage=1
-
-
-//https://youtu.be/umepbfKp5rI?t=17011
+//https://youtu.be/umepbfKp5rI?t=15149
+//https://coinsbench.com/walkthrough-using-chainlink-data-feeds-for-computation-in-cryptozombies-71ac9bb7abba 
 //https://youtu.be/umepbfKp5rI?t=21093
-//eth-converter.com
+//Start MetaMask bofore llstarting Remix
+// In remix you sould chose enviroment :'Injected provider - Metamask'
+
+
 // Get funds from 
 // Withdraw funds
 // Set a minimum  funding value in USD
 
-
 pragma solidity ^0.8.18;
-
-//browser extension - injected provider metamask
-// Note: The AggregatorV3Interface might be at a different location than what was in the video!
-
-//https://coinsbench.com/walkthrough-using-chainlink-data-feeds-for-computation-in-cryptozombies-71ac9bb7abba
-
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+//import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
-
 error NotOwner();
-
 contract FundMe {
     using PriceConverter for uint256;
+    mapping(address => uint256 amountFounded) public addressToAmountFunded;
+   
 
-    mapping(address => uint256) public addressToAmountFunded;
+    uint256 public constant MINIMUM_USD = 5 * 10e18; //5$   
     address[] public funders;
-
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
-
-    constructor() {
+    // Could we make this constant?    no! We should make it immutable! */
+    // Becase we set it not in the same line of declration.
+    address public immutable i_owner;
+  
+   constructor() {
         i_owner = msg.sender;
     }
 
- //In order for a function to recive native blockchin token it must
- // be marked  as payable
 
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
-    }
+ //In order for a function to recive native blockchin token it must  be marked  as payable
+ function fund() public  payable {  
+    
+    require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+    //  require(msg.value > 1e16, "You need to spend more ETH!"); 
+     //require(getConversionRate(msg.value ) >= MINIMUM_USD, "You need to spend more ETH!");
+     addressToAmountFunded[msg.sender] += msg.value;
+     funders.push(msg.sender);  
+  } 
 
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
 
-    modifier onlyOwner() {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
-    }
-
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+  function withdraw() public onlyOwner {
+  
+     //require(msg.sender == i_owner,"Must be owner!");
+     for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
-        }
-        funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
+     }
+     //Reset the array length of 0
+     funders = new address[](0);
+    //                      Withdraw funds
+    //                  =======================
+    //                there are 3 ways: transfer, send, call
+   //  1. transfer
+     // msg.sender is of type:    address  
+     // payable(msg.sender) is of type:     payable address
+    // payable(msg.sender).transfer(address(this).balance);
 
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
+   // 2. send
+   // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+   // require(sendSuccess, "Send failed"); // if sendSuccess is false?
+   
+    // 3. call
+    (bool callSuccess,  ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(callSuccess, "Call failed");
 
-        // call
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
+  } 
+ 
     // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+
+ modifier onlyOwner() {
+     
+      //  require(msg.sender == i_owner,"Must be owner!");
+    if (msg.sender != i_owner) revert NotOwner();
+    // error without message saves gas
+        _; //Execute the code inside the function that this modifer is used 
+    }
+
+     // Explainer from: https://solidity-by-example.org/fallback/
     // Ether is sent to contract
     //      is msg.data empty?
     //          /   \
@@ -116,3 +122,7 @@ contract FundMe {
 
 
 
+
+
+
+}
