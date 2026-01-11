64ff95cdc493b14aa459fae15951d075b7e82587
danizalm05 <danizalm05@gmail.com>
2026-01-11 16:58:53 +0200

    29067

---
+++ networks/Network code/chapter02/big_sender.py
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env python3
+# Sending a Large UDP Packet
+# Foundations of Python Network Programming, Third Edition  page 32
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter02/big_sender.py
+# Send a big UDP datagram to learn the MTU of the network path.
+import IN, argparse, socket
