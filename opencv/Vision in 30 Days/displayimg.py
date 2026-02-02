011e715da90f64f52fc437b798f27b89be90a062
danizalm05 <danizalm05@gmail.com>
2026-02-02 17:24:47 +0200

    15438

---
+++ opencv/Vision in 30 Days/displayimg.py
@@ -0,0 +1,30 @@
+"""
+Display Image
+https://docs.opencv.org/4.x/d2/d96/tutorial_py_table_of_contents_imgproc.html
+https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html 
+https://docs.opencv.org/4.x/db/deb/tutorial_display_image.html 
+
+https://www.youtube.com/watch?v=eDIj5LuIL4A&list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&index=2
+"""
+
+import cv2 as cv
+import sys
+import getpass
+
+img='1.jpg'
+BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
+path = BASE_FOLDER+img
+print("Image  = ",path) 
+print("Image  shape  = ",path) 
+img = cv.imread(path)
+print("Image  shape  = ",img.shape) 
+cv.imshow("Display window", img)
+key = cv.waitKey(0)
+'''
+if img is None:
+    sys.exit("Could not read the image.")
+cv.imshow("Display window", img)
+key = cv.waitKey(1)
+#cv2.destroyAllWindows()
+
+'''
