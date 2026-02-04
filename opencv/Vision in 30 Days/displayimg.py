b3aeecc20eef8bf339191a7957b393b51725df03
danizalm05 <danizalm05@gmail.com>
2026-02-04 17:20:48 +0200

    21890

--- opencv/Vision in 30 Days/displayimg.py
+++ opencv/Vision in 30 Days/displayimg.py
@@ -1,7 +1,6 @@
 """
 
-https://marketplace.visualstudio.com/items?itemName=ms-python.python
-https://code.visualstudio.com/docs/python/python-tutorial#_prerequisites
+
 Display Image
 https://docs.opencv.org/4.x/d2/d96/tutorial_py_table_of_contents_imgproc.html
 https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html 
