ffe56e08707941861ec2fa88b8f930b1d2a39bc7
danizalm05 <danizalm05@gmail.com>
2026-02-17 16:36:20 +0200

    11063

--- opencv/Vision in 30 Days/threshold.py
+++ opencv/Vision in 30 Days/threshold.py
@@ -1,17 +1,21 @@
 '''
                                 Threshold
 https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=4017
-https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t= 
-https://github.com/jasmcaus/opencv-course/blob/master/Section%20%232%20-%20Advanced/blurring.py
+https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=4344
+
+https://github.com/jasmcaus/opencv-course/blob/master/Section%20%231%20-%20Basics/thresh.py
+
 https://docs.opencv.org/4.x/
-https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html#autotoc_md1369
+https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
+
+
 '''
 
 import cv2 as cv
 #import sys
 import getpass
 
-img='1.jpg'
+img='2.jpg'
 BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
 path = BASE_FOLDER+img
 print("Image  = ",path) 
@@ -28,11 +32,13 @@ assert img is not None, "file could not be read, check with os.path.exists()"
  
 k_size = 7
 
-# Averaging
-average = cv.blur(img, (k_size,k_size ))
+# Gray
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
 
-# Gaussian Blur
-Gaussianblur = cv.GaussianBlur(img,(k_size,k_size),3)
+
+# Simple Thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY )
 
 
 # .median Blur  ( effective against salt-and-pepper)
@@ -43,8 +49,8 @@ kernel = np.ones((5,5),np.float32)/25
 dst = cv.filter2D(img,-1,kernel)
 
 plt.subplot(231),plt.imshow(img),plt.title('Original')
-plt.subplot(232),plt.imshow(average),plt.title('average')
-plt.subplot(233),plt.imshow(Gaussianblur),plt.title('Gaussian') 
+plt.subplot(232),plt.imshow(gray),plt.title('gray')
+plt.subplot(233),plt.imshow(thresh),plt.title('bin-thresh') 
 plt.subplot(234),plt.imshow(median),plt.title('median') 
 plt.subplot(236),plt.imshow(dst),plt.title('Averaging')
 plt.xticks([]), plt.yticks([])
@@ -52,8 +58,8 @@ plt.show()
  
 
 cv.imshow("Display window", img)
-cv.imshow(" average" , average)
-cv.imshow(" Gaussianblur" , Gaussianblur)
+cv.imshow(" gray" , gray)
+cv.imshow(" Gaussianblur" , thresh)
 cv.imshow(" median" , median)
 
 cv.waitKey(0)
