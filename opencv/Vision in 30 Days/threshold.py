7e963daf54d864fb3f01f62ec411dbaa39a4f694
danizalm05 <danizalm05@gmail.com>
2026-02-18 17:26:57 +0200

    8215

--- opencv/Vision in 30 Days/threshold.py
+++ opencv/Vision in 30 Days/threshold.py
@@ -1,66 +1,54 @@
 '''
                                 Threshold
 https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=4017
-https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=4344
+https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=4933
 
 https://github.com/jasmcaus/opencv-course/blob/master/Section%20%231%20-%20Basics/thresh.py
 
 https://docs.opencv.org/4.x/
 https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
 
-
 '''
 
 import cv2 as cv
-#import sys
 import getpass
+ 
 
-img='2.jpg'
+img=  'sudoku.png'  #'2.jpg'
 BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
 path = BASE_FOLDER+img
 print("Image  = ",path) 
 print("Image  shape  = ",path) 
 
+def rescaleFrame(frame, scale=0.75):
+    # Images, Videos and Live Video
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA) 
 
 
-import numpy as np
- 
-from matplotlib import pyplot as plt
- 
 img = cv.imread(path)
 assert img is not None, "file could not be read, check with os.path.exists()"
  
-k_size = 7
-
 # Gray
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
-
+ 
 
 # Simple Thresholding
-threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY )
+threshold, thresh = cv.threshold(gray, 80, 255, cv.THRESH_BINARY )
 
-
-# .median Blur  ( effective against salt-and-pepper)
-median = cv.medianBlur(img,k_size)
-
+# Addaptive  Thresholding 
+th3 = cv.adaptiveThreshold(gray, 255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY,29,30)
  
-kernel = np.ones((5,5),np.float32)/25
-dst = cv.filter2D(img,-1,kernel)
-
-plt.subplot(231),plt.imshow(img),plt.title('Original')
-plt.subplot(232),plt.imshow(gray),plt.title('gray')
-plt.subplot(233),plt.imshow(thresh),plt.title('bin-thresh') 
-plt.subplot(234),plt.imshow(median),plt.title('median') 
-plt.subplot(236),plt.imshow(dst),plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-plt.show()
+scl = .7
+cv.imshow("Source", rescaleFrame(img,  scale = scl))
+cv.imshow(" gray" , rescaleFrame(gray,  scale = scl) )
+cv.imshow(" global thresh" , rescaleFrame(thresh,  scale = scl) )
+cv.imshow("ADAPTIVE_THRESH_GAUSSIAN_C", rescaleFrame(th3,  scale = scl ))
  
-
-cv.imshow("Display window", img)
-cv.imshow(" gray" , gray)
-cv.imshow(" Gaussianblur" , thresh)
-cv.imshow(" median" , median)
 
 cv.waitKey(0)
 cv.destroyAllWindows() 
