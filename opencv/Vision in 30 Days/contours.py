bd982ce871fab3bf136885d691d692da31392738
Richm <ric5hme45@yahoo.com>
2026-03-04 13:06:53 +0200

    7915

--- opencv/Vision in 30 Days/contours.py
+++ opencv/Vision in 30 Days/contours.py
@@ -15,7 +15,7 @@ from contourInp import   inpTrackbar
 
 inpWinName = "Input"
 inpTrackbar(inpWinName)
-img=  '3.jpg'  #'2.jpg'
+img=  '1.jpg'  #'2.jpg'
 BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
 path = BASE_FOLDER+img
 print("Image  = ",path) 
@@ -40,24 +40,29 @@ while True:
   
 
     contours, hierarchy = cv.findContours(threshImg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    print("Total Number of contours:" + str(len(contours)))
     i=0
     imgout = imgs.copy()
+    imgout2 = imgs.copy()
     for cnt in contours:
         if cv.contourArea(cnt) > cnt_area:
            i += 1
-           print(i,".  " ,cv.contourArea(cnt))
-           cv.drawContours( imgout, cnt, -1, (0,255,0), 2)
-    
+           #print(i,".  " ,cv.contourArea(cnt))
+           cv.drawContours( imgout, cnt, -1, (0,255,0), 3)
+           x1,y1,w,h = cv.boundingRect(cnt)
+           cv.rectangle(imgout2,(x1,y1),(x1+w,y1+h),(255,0,0),3)
+    print("Number of contours:", i)
     imgg = img.copy()
-    PutTextOnImage(imgs,"source")
+    #PutTextOnImage(imgs,"source")
     PutTextOnImage(imgg,"gray")
     PutTextOnImage( threshImg,"thresh")
-    
+    PutTextOnImage( imgout,"imgout")
+    PutTextOnImage( imgout2,"imgout2")
     
     
     imgStack = stackImages(scl,
                           ( [imgs, imgg, threshImg],
-                            [ imgout, imgg, imgg]
+                            [ imgout, imgout2, imgg]
                           )    )
  
     cv.imshow("ImageStack", imgStack)
