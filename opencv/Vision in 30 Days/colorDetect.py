fef767fb519b4b7b2fdff1beadbb005ba756f0b4
danizalm05 <danizalm05@gmail.com>
2026-03-06 21:44:11 +0200

    18201

--- opencv/Vision in 30 Days/colorDetect.py
+++ opencv/Vision in 30 Days/colorDetect.py
@@ -1,6 +1,9 @@
 '''
 Detecting color with Python and OpenCV using HSV colorspace  
-https://www.youtube.com/watch?v=aFNDh5k3SjU&list=PLb49csYFtO2Hpfn8eLnaD9tJ0xYcMVcWe
+
+https://www.youtube.com/watch?v=eDIj5LuIL4A&list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=7094s
+https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=8486
+
 '''
 
 
@@ -24,8 +27,9 @@ camera_num = 0
 cap = cv2.VideoCapture(camera_num)
 
 while True:
+  
     scale = cv2.getTrackbarPos("scale", inpWinName) / 10
-
+    '''
      # Input BGR  values of the wanted color
     Red = cv2.getTrackbarPos("Red", inpWinName)
     Green = cv2.getTrackbarPos("Green", inpWinName)
@@ -33,9 +37,14 @@ while True:
 
     chosenColor = [Red, Green, Blue]  # chosenColor in BGR colorspace
     # ----------------------
+    '''
+
     ret, frame = cap.read()
+    
     colorBox = frame.copy()
+     
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    '''  
     lowerLimit, upperLimit = get_limits(chosenColor)
 
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
@@ -44,22 +53,25 @@ while True:
     if bbox is not None:
         x1, y1, x2, y2 = bbox
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-
+    '''
     cv2.imshow('frame', frame)
-
+    '''
     start_point = (25, 25)
     end_point = (320, 320)
     color = (255, 55, 0)
     thickness = -1
     colorBox = cv2.rectangle(colorBox, start_point, end_point, chosenColor, thickness)
     PutTextOnImage(frame,"txt")
-    imgStack = stackImages( scale,  ([colorBox], [frame ])    )
+
+        '''    
+    imgStack = stackImages( scale,  ([colorBox], [frame ])    )   
+ 
+
+
     cv2.imshow("ImageStack", imgStack)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         
         break
-
 cap.release()
- 
 cv2.destroyAllWindows()
 
