43fb7e998a4eb7bfc890f98dde0247291ad3b00b
Richm <ric5hme45@yahoo.com>
2026-03-08 13:15:34 +0200

    24088

--- opencv/Vision in 30 Days/colorDetectInp.py
+++ opencv/Vision in 30 Days/colorDetectInp.py
@@ -28,8 +28,10 @@ def inpTrackbar(winName):
 def get_limits(color):
 
     c = np.uint8([[color]])  # here insert the bgr values which you want to convert to hsv
+ 
+    
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-   
+    print ( color , hsvC)
     lowerLimit = hsvC[0][0][0] - 10, 100, 100
     upperLimit = hsvC[0][0][0] + 10, 255, 255
 
--- opencv/Vision in 30 Days/untitled0.py
+++ opencv/Vision in 30 Days/untitled0.py
@@ -1,8 +1,6 @@
 '''
 Detecting color with Python and OpenCV using HSV colorspace  
-
-https://www.youtube.com/watch?v=eDIj5LuIL4A&list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=7094s
-https://youtu.be/eDIj5LuIL4A?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=8354
+https://www.youtube.com/watch?v=aFNDh5k3SjU&list=PLb49csYFtO2Hpfn8eLnaD9tJ0xYcMVcWe
 '''
 
 
@@ -10,10 +8,6 @@ import cv2
 from PIL import Image
 from outputCV import stackImages,PutTextOnImage
 from colorDetectInp import  inpTrackbar ,get_limits
- 
-
-
-
 inpWinName = "Input"
 
 
@@ -26,58 +20,39 @@ camera_num = 0
 cap = cv2.VideoCapture(camera_num)
 
 while True:
-  
     scale = cv2.getTrackbarPos("scale", inpWinName) / 10
-    
+
      # Input BGR  values of the wanted color
     Red = cv2.getTrackbarPos("Red", inpWinName)
     Green = cv2.getTrackbarPos("Green", inpWinName)
     Blue = cv2.getTrackbarPos("Blue", inpWinName)
 
     chosenColor = [Red, Green, Blue]  # chosenColor in BGR colorspace
-    print(chosenColor)
-    
- 
     # ----------------------
-     
-
     ret, frame = cap.read()
-    
     colorBox = frame.copy()
-     
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lowerLimit, upperLimit = get_limits(chosenColor)
+
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
     mask_ = Image.fromarray(mask)
-
-
-    '''  
-   
-
-      mask_ = Image.fromarray(mask)
     bbox = mask_.getbbox()
     if bbox is not None:
         x1, y1, x2, y2 = bbox
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-    '''
-    cv2.imshow('Source', frame)
-    '''
+
+    cv2.imshow('frame', frame)
+
     start_point = (25, 25)
     end_point = (320, 320)
     color = (255, 55, 0)
     thickness = -1
     colorBox = cv2.rectangle(colorBox, start_point, end_point, chosenColor, thickness)
-    PutTextOnImage(frame,"txt")
-
-        '''    
-    imgStack = stackImages( scale,  ([hsvImage],[mask],[colorBox], [frame ])    )   
- 
-
-
+    imgStack = stackImages( scale,  ([colorBox], [frame ])    )
     cv2.imshow("ImageStack", imgStack)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        
         break
+
 cap.release()
 cv2.destroyAllWindows()
 
