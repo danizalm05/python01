e128ac38a31d22ed87e0516683edd21ed8cdb692
danizalm05 <danizalm05@gmail.com>
2026-03-27 22:18:52 +0300

    7296

--- opencv/Vision in 30 Days/text-detection.py
+++ opencv/Vision in 30 Days/text-detection.py
@@ -2,7 +2,11 @@
 """
 text-detection
 www.youtube.com/watch?v=CcC3h0waQ6I&list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&index=5
+https://youtu.be/CcC3h0waQ6I?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=388
 https://github.com/computervisioneng/text-detection-python-tesseract-easyocr-textract/tree/main
+
+https://bgshih.github.io/cocotext/
+https://www.pexels.com/         search for text
 """
 import cv2
 import easyocr
