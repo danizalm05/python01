8bf2785c9236ca747c8697db8b74b14b56ee7139
danizalm05 <danizalm05@gmail.com>
2026-03-28 17:44:22 +0300

    6285

---
+++ opencv/Vision in 30 Days/img-classi-scikit.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+    image-classification-python-scikit-learn
+https://youtu.be/il8dMDlXrIE?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=339
+
+https://github.com/computervisioneng/image-classification-python-scikit-learn/blob/master/main.py
+
+data:
+  https://drive.google.com/drive/folders/1CjEFWihRqTLNUnYRwHXxGAVwSXF2k8QC
+"""
+
+import getpass
+
+import os
+import pickle
+
+from skimage.io import imread
+from skimage.transform import resize
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+# prepare data
+
+categories = ['empty', 'not_empty']
+input_dir = 'C:/Users/' + getpass.getuser() + '/Pictures/clf-data'
+ 
+#C:\Users\rockman\Pictures\clf-data
+data = []
+labels = [] 
+
+for category_idx, category in enumerate(categories):
+      catg = (os.path.join(input_dir, category))
+      for file in os.listdir(catg  ):
+          img_path = os.path.join(input_dir, catg, file)
+          print(img_path)
+     
--- opencv/Vision in 30 Days/text-detection.py
+++ opencv/Vision in 30 Days/text-detection.py
@@ -11,4 +11,31 @@ https://www.pexels.com/         search for text
 import cv2
 import easyocr
 import matplotlib.pyplot as plt
-import numpy as np
+import getpass
+
+img=  'txt1.jpg'  #'2.jpg'
+BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
+path = BASE_FOLDER+img
+print("Image  = ",path) 
+print("Image  shape  = ",path) 
+img = cv2.imread(path)
+
+# instance text detector
+reader = easyocr.Reader(['en'], gpu=False)
+
+# detect text on image
+text_ = reader.readtext(img)
+
+threshold = 0.25
+# draw bbox and text
+for t_, t in enumerate(text_):
+    #print(t)
+
+    bbox, text, score = t
+
+    if score > threshold:
+        cv2.rectangle(img, [int(j) for j in bbox[0]], [int(j) for j in bbox[2]], (0, 255, 0), 15)
+        cv2.putText(img, text, [int(j) for j in bbox[0]], cv2.FONT_HERSHEY_COMPLEX, 2.5, (0, 0, 255), 5)
+        print(text)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
