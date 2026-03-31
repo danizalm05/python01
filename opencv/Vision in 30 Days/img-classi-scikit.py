c254e920752ed6c3f0ddbd8956bfe2ae57ff5117
danizalm05 <danizalm05@gmail.com>
2026-03-31 17:10:02 +0300

    26799

--- opencv/Vision in 30 Days/img-classi-scikit.py
+++ opencv/Vision in 30 Days/img-classi-scikit.py
@@ -36,7 +36,7 @@ for category_idx, category in enumerate(categories):
       catg = (os.path.join(input_dir, category))
       for file in os.listdir(catg  ):
           img_path = os.path.join(input_dir, catg, file)
-          #print(file)
+          print(file)
           img = imread(img_path)
           img = resize(img, (15, 15)) 
           data.append(img.flatten())
@@ -45,5 +45,26 @@ data = np.asarray(data)
 labels = np.asarray(labels)
 
 # train / test split
+print("\n------------------ \nsplit  \n")
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
-       
+ 
+# train classifier
+classifier = SVC()
+
+parameters = [{'gamma': [0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}]
+
+print("\n------------------ \ntrain  \n")
+grid_search = GridSearchCV(classifier, parameters)
+
+grid_search.fit(x_train, y_train)
+  
+# test performance
+best_estimator = grid_search.best_estimator_
+
+y_prediction = best_estimator.predict(x_test)
+
+score = accuracy_score(y_prediction, y_test)
+
+print('{}% of samples were correctly classified'.format(str(score * 100)))
+
+pickle.dump(best_estimator, open('./model.p', 'wb'))
---
+++ opencv/Vision in 30 Days/parking-counter.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+  https://www.youtube.com/watch?v=F-884J2mnOY&list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&index=15  
+  https://github.com/computervisioneng/parking-space-counter
+data:
+  https://drive.google.com/drive/folders/1CjEFWihRqTLNUnYRwHXxGAVwSXF2k8QC
+"""
+ 
