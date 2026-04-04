8c350228f4cc0a54bd4099f78c8ee0a7cce491c6
danizalm05 <danizalm05@gmail.com>
2026-04-04 17:05:20 +0300

    7580

--- opencv/Vision in 30 Days/Parking spot/parking-counter.py
+++ opencv/Vision in 30 Days/Parking spot/parking-counter.py
@@ -7,7 +7,7 @@
   https://github.com/computervisioneng/parking-space-counter
 
 data:
-  https://drive.google.com/drive/folders/1CjEFWihRqTLNUnYRwHXxGAVwSXF2k8QC
+  https://drive.googQle.com/drive/folders/1CjEFWihRqTLNUnYRwHXxGAVwSXF2k8QC
   masks:
    https://github.com/computervisioneng/parking-space-counter/blob/master/mask_1920_1080.png
    https://github.com/computervisioneng/parking-space-counter/blob/master/mask_crop.png
@@ -15,7 +15,9 @@ data:
 """
 import cv2 
 import getpass
- 
+from util import get_parking_spots_bboxes, empty_or_not
+
+
 #Read the mask image
 img=  'mask_crop.png'  
 MASK_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
@@ -29,9 +31,8 @@ input_file = VID_FOLDER + vid_name
 
 
 cap = cv2.VideoCapture(input_file)
-#connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
-connected_components = cv2.connectedComponents(mask, 4, cv2.CV_32S)
-
+connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+spots = get_parking_spots_bboxes(connected_components)
 
 # Check if camera opened successfully:
 if cap.isOpened()is False:
@@ -42,9 +43,17 @@ step = 30
 while ret:
     ret, frame = cap.read()
     
-    
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    for spot_indx, spot in enumerate(spots):
+        x1, y1, w, h = spot  
+        frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)       
+
+
+    #cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.imshow('frame', frame)   
+    if cv2.waitKey(5) & 0xFF == ord('q'):
        break
+   
+    
+   
 cap.release()
 cv2.destroyAllWindows()    
