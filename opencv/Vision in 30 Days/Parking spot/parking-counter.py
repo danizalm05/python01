03ca08707c7dad2711026544876593ab5e3160b3
danizalm05 <danizalm05@gmail.com>
2026-04-06 17:23:26 +0300

    18359

--- opencv/Vision in 30 Days/Parking spot/parking-counter.py
+++ opencv/Vision in 30 Days/Parking spot/parking-counter.py
@@ -2,8 +2,8 @@
 """
   
   https://www.youtube.com/watch?v=F-884J2mnOY&list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&index=16
- 
-  https://youtu.be/F-884J2mnOY?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=954
+  https://youtu.be/F-884J2mnOY?list=PLb49csYFtO2HAdNGChGzohFJGnJnXBOqd&t=1830 
+  
   https://github.com/computervisioneng/parking-space-counter
 
 data:
@@ -16,7 +16,12 @@ data:
 import cv2 
 import getpass
 from util import get_parking_spots_bboxes, empty_or_not
+import matplotlib.pyplot as plt
+import numpy as np
 
+
+def calc_diff(im1, im2):
+    return np.abs(np.mean(im1) - np.mean(im2))
 
 #Read the mask image
 img=  'mask_crop.png'  
@@ -29,31 +34,75 @@ VID_FOLDER = 'C:/Users/'+ getpass.getuser() +'/Videos/'
 vid_name = "parking_crop_loop.mp4" 
 input_file = VID_FOLDER + vid_name
 
-
 cap = cv2.VideoCapture(input_file)
-connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
-spots = get_parking_spots_bboxes(connected_components)
-
-# Check if camera opened successfully:
+# Check if video opened successfully:
 if cap.isOpened()is False:
     print("Error opening video stream or file")
 
+ 
+
+connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+
+spots = get_parking_spots_bboxes(connected_components)
+
+spots_status = [None for j in spots]
+diffs = [None for j in spots]
+
+previous_frame = None
+
+frame_nmr = 0
 ret = True
 step = 30
 while ret:
     ret, frame = cap.read()
-    
+
+    if frame_nmr % step == 0 and previous_frame is not None:
+        for spot_indx, spot in enumerate(spots):
+            x1, y1, w, h = spot
+
+            spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
+
+            diffs[spot_indx] = calc_diff(spot_crop, previous_frame[y1:y1 + h, x1:x1 + w, :])
+
+        print([diffs[j] for j in np.argsort(diffs)][::-1])
+
+    if frame_nmr % step == 0:
+        if previous_frame is None:
+            arr_ = range(len(spots))
+        else:
+            arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs) > 0.4]
+        for spot_indx in arr_:
+            spot = spots[spot_indx]
+            x1, y1, w, h = spot
+
+            spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
+
+            spot_status = empty_or_not(spot_crop)
+
+            spots_status[spot_indx] = spot_status
+
+    if frame_nmr % step == 0:
+        previous_frame = frame.copy()
+
     for spot_indx, spot in enumerate(spots):
-        x1, y1, w, h = spot  
-        frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)       
+        spot_status = spots_status[spot_indx]
+        x1, y1, w, h = spots[spot_indx]
 
+        if spot_status:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+        else:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
 
-    #cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-    cv2.imshow('frame', frame)   
-    if cv2.waitKey(5) & 0xFF == ord('q'):
-       break
-   
-    
-   
+    cv2.rectangle(frame, (80, 20), (550, 80), (0, 0, 0), -1)
+    cv2.putText(frame, 'Available spots: {} / {}'.format(str(sum(spots_status)), str(len(spots_status))), (100, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+    frame_nmr += 1
+
 cap.release()
-cv2.destroyAllWindows()    
+cv2.destroyAllWindows() 
