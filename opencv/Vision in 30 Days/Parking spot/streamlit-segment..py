65e167a46fe5ad54b9948ce45cb3ff660045ef24
danizalm05 <danizalm05@gmail.com>
2026-04-09 21:39:41 +0300

    30272

---
+++ opencv/Vision in 30 Days/Parking spot/streamlit-segment..py
@@ -0,0 +1,13 @@
+'''
+
+Machine learning web app with Python, Streamlit & Segment Anything Model
+ | Modelbit model deployment 
+
+https://www.youtube.com/watch?v=W8OvdQPL7lk&list=PLb49csYFtO2HGELdc-RLRCNVNy0g2UMwc&index=21
+
+https://github.com/computervisioneng/streamlit-segment-anything-model-python/blob/main/BackgroundRemovalWebaPP_Backend.ipynb
+
+colab address 
+https://colab.research.google.com/drive/109oevnjECkL-tG5z5T3KPG70wIkjjtAj#scrollTo=2LozyVi_i6rE
+
+'''
