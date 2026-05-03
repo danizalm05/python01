d8887b400f3b59ef8f2c4257aec86e785a3b55dd
danizalm05 <danizalm05@gmail.com>
2026-05-03 21:58:45 +0300

    22541

--- opencv/Vision in 30 Days/sign-language.py
+++ opencv/Vision in 30 Days/sign-language.py
@@ -1,7 +1,19 @@
 
-
+'''
 
 https://github.com/computervisioneng/sign-language-detector-python
 https://www.youtube.com/watch?v=MJCSjXepaAM&list=PLb49csYFtO2HGELdc-RLRCNVNy0g2UMwc&index=3
 
+'''
+import pickle
 
+import cv2 
+
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import hand_landmarker
+ 
+from mediapipe.tasks.python.vision import RunningMode
+import cv2
+import numpy as np
+
+print(mp.__version__)  
