65d3d02cbea6657cba0f0b3596d0c0596ea5081a
danizalm05 <danizalm05@gmail.com>
2026-05-04 17:13:45 +0300

    19466

---
+++ opencv/Vision in 30 Days/yolov8 plate.py
@@ -0,0 +1,13 @@
+ 
+'''
+       Automatic number plate recognition with Python, Yolov8 and EasyOCR  
+https://www.youtube.com/watch?v=fyJB1t0o0ms
+
+https://github.com/computervisioneng/automatic-number-plate-recognition-python-yolov8
+
+'''
+
+
+import cv2
+import torch
+from ultralytics import YOLOv10 as YOLO
