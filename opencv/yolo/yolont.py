feff0154876b70cbac3c2c9c35f8c6623772df55
danizalm05 <danizalm05@gmail.com>
2026-05-16 17:35:49 +0300

    1667

--- opencv/yolo/yolont.py
+++ opencv/yolo/yolont.py
@@ -12,7 +12,24 @@ https://github.com/computervisioneng/train-yolov8-custom-dataset-step-by-step-gu
 https://github.com/ultralytics/ultralytics
 
 pip install ultralytics
-'''
+
+ You need to create "config.yaml" https://github.com/computervisioneng/train-yolov8-custom-dataset-step-by-step-guide/blob/master/local_env/config.yaml
+ ---------------   config.yaml   -----------------
+path: /home/........./code/data  # dataset root dir
+train: images/train  # train images (relative to 'path')
+val: images/train  # val images (relative to 'path')
+
+# Classes
+names:
+  0: alpaca
+  
+---------------  
+
+uselly the vlidation image sould point to another group of images to do
+the validation process but here we are using the same dir to make the preocss
+simpler.
+''' 
+ 
 
 
 from ultralytics import YOLO
@@ -21,14 +38,5 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-#You need to create "config.yaml" https://github.com/computervisioneng/train-yolov8-custom-dataset-step-by-step-guide/blob/master/local_env/config.yaml
-'''
-path: /home/........./code/data  # dataset root dir
-train: images/train  # train images (relative to 'path')
-val: images/train  # val images (relative to 'path')
-'''
-# Classes
-names:
-  0: alpaca
-'''
+
 results = model.train(data="config.yaml", epochs=1)  # train the model
