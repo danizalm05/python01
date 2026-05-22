d89747908db133472fb512bc09ff34956bb20b19
danizalm05 <danizalm05@gmail.com>
2026-05-22 22:59:49 +0300

    21319

--- opencv/yolo/number-plate/main.py
+++ opencv/yolo/number-plate/main.py
@@ -1,17 +1,6 @@
 
 '''
  automatic-number-plate-recognition-python
- https://youtu.be/fyJB1t0o0ms?t=458
- https://github.com/computervisioneng/automatic-number-plate-recognition-python-yolov8/blob/main/main.py
+ https://www.youtube.com/watch?v=k4OIaCOH9NY
 ''' 
-
-from ultralytics import YOLO
-import cv2
  
-# load models
-coco_model = YOLO('yolov8n.pt')
-license_plate_detector = YOLO('./models/license_plate_detector.pt')
-
-
-# load video
-cap = cv2.VideoCapture('./sample.mp4')
