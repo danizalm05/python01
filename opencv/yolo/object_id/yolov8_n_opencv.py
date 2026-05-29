c62a5dde0f366c0ee715511407f8b5c1e8e9fd10
danizalm05 <danizalm05@gmail.com>
2026-05-29 17:54:05 +0300

    3132

--- opencv/yolo/object_id/yolov8_n_opencv.py
+++ opencv/yolo/object_id/yolov8_n_opencv.py
@@ -17,10 +17,11 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-img='cats.jpg'
-BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
-path = BASE_FOLDER+img
-print("Image  = ",path) 
+vid= 'afriq1.MP4' # 'los_angeles.mp4'   'dog.mp4''afriq0.MP4'
+BASE_FOLDER = 'C:/Users/'+ getpass.getuser() +'/Videos/'
+#BASE_FOLDER = 'C:/Users/' + getpass.getuser() + '/Pictures/Saved Pictures/'
+video_name = BASE_FOLDER+vid
+print("Image  = ",video_name ) 
 
 # opening the file in read mode
 # A list of objects
@@ -51,7 +52,7 @@ frame_wid = 640
 frame_hyt = 480
 
 # cap = cv2.VideoCapture(1)
-cap = cv2.VideoCapture("inference/videos/afriq0.MP4")
+cap = cv2.VideoCapture(video_name )
 
 if not cap.isOpened():
     print("Cannot open video")
@@ -61,6 +62,46 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     
+    if not ret:
+       print("Can't receive frame (stream end?). Exiting ...")
+       break
+    # Predict on image
+    detect_params = model.predict(source=[frame], conf=0.45, save=False)
+
+    # Convert tensor array to numpy
+    DP = detect_params[0].numpy()
+    print(DP)
+    
+    if len(DP) != 0:
+        for i in range(len(detect_params[0])):
+            print(i)
+
+            boxes = detect_params[0].boxes
+            box = boxes[i]  # returns one box
+            clsID = box.cls.numpy()[0]
+            conf = box.conf.numpy()[0]
+            bb = box.xyxy.numpy()[0]
+
+            cv2.rectangle(
+                frame,
+                (int(bb[0]), int(bb[1])),
+                (int(bb[2]), int(bb[3])),
+                detection_colors[int(clsID)],
+                3,
+            )
+
+            # Display class name and confidence
+            font = cv2.FONT_HERSHEY_COMPLEX
+            cv2.putText(
+                frame,
+                class_list[int(clsID)] + " " + str(round(conf, 3)) + "%",
+                (int(bb[0]), int(bb[1]) - 10),
+                font,
+                1,
+                (255, 255, 255),
+                2,
+            )
+
     # Display the resulting frame
     cv2.imshow("ObjectDetection", frame)
 
