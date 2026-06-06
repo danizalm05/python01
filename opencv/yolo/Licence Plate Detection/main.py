c30866cb8d12cf4f4510a3dd5451a07284beee46
danizalm05 <danizalm05@gmail.com>
2026-06-06 17:31:16 +0300

    27606

--- opencv/yolo/Licence Plate Detection/main.py
+++ opencv/yolo/Licence Plate Detection/main.py
@@ -1,6 +1,11 @@
 
 '''
  automatic-number-plate-recognition-python
- https://www.youtube.com/watch?v=k4OIaCOH9NY
+ https://www.youtube.com/watch?v=zh1CZK7chbo&list=PLmxueiA72TthnD-ElEeVVXbw44Ij2pETT&index=3
+ 
+ https://github.com/Arijit1080/Licence-Plate-Detection-using-YOLO-V8
+ URL of the data used in this project
+ https://universe.roboflow.com/mochoye/license-plate-detector-ogxxg
+
 ''' 
  
