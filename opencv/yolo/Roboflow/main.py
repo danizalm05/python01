17e364a740a3625a6fbb24068883787ec3e957b7
danizalm05 <danizalm05@gmail.com>
2026-06-13 17:34:59 +0300

    4492

--- opencv/yolo/Roboflow/main.py
+++ opencv/yolo/Roboflow/main.py
@@ -1,6 +1,12 @@
 
 '''
-https://www.youtube.com/@Roboflow
-https://colab.research.google.com/github/roboflow-ai/notebooks/blob/main/notebooks/train-yolov8-object-detection-on-custom-dataset.ipynb#scrollTo=tdSMcABDNKW-
+
+YOLOv8: How to Train for Object Detection on a Custom Dataset 
+Roboflow
+https://www.youtube.com/watch?v=wuZtUMEiKWY 
+https://colab.research.google.com/github/roboflow-ai/notebooks/blob/main/notebooks/train-yolov8-object-detection-on-custom-dataset.ipynb
+https://blog.roboflow.com/how-to-train-yolov8-on-a-custom-dataset/
+
+
 ''' 
  
