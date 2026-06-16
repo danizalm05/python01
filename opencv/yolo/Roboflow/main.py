cdc5194803053878115826471c2e0b26ef17013a
danizalm05 <danizalm05@gmail.com>
2026-06-16 17:33:48 +0300

    31511

--- opencv/yolo/Roboflow/main.py
+++ opencv/yolo/Roboflow/main.py
@@ -3,10 +3,16 @@
 
 YOLOv8: How to Train for Object Detection on a Custom Dataset 
 Roboflow
-https://www.youtube.com/watch?v=wuZtUMEiKWY 
-https://colab.research.google.com/github/roboflow-ai/notebooks/blob/main/notebooks/train-yolov8-object-detection-on-custom-dataset.ipynb
+https://www.youtube.com/watch?v=wuZtUMEiKWY  
+https://colab.research.google.com/drive/14-rPGxdUi3fK51YCt3w72MPtDZnR_-1g
+https://universe.roboflow.com/
+
 https://blog.roboflow.com/how-to-train-yolov8-on-a-custom-dataset/
 
 
+https://universe.roboflow.com/
+1. login
+2. create 
+3. Chose objext detection
 ''' 
  
---
+++ opencv/yolo/y8-datbase/main.py
@@ -0,0 +1,8 @@
+
+'''
+         Train Yolov8 custom dataset on Google Colab  
+         Felipe Tambasco
+ https://www.youtube.com/watch?v=bx52WmQvbaE
+ https://github.com/computervisioneng/train-yolov8-object-detector-google-drive-google-colab/blob/main/TrainYolov8CustomDataset.ipynb
+''' 
+ 
