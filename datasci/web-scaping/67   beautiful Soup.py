ef9df465436c53b66ba7ac3195b9d34d373758e4
danizalm05 <danizalm05@gmail.com>
2026-09-21 18:18:43 +0300

    14437

--- datasci/web-scaping/67   beautiful Soup.py
+++ datasci/web-scaping/67   beautiful Soup.py
@@ -1,18 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-url = 'https://news.walla.co.il/item/3868579'
+
+url = 'https://linuxsimply.com/linux-basics/os-installation/virtual-machine/kali-linux-on-virtualbox/'
+url1 = 'https://news.walla.co.il/item/3868579'
 r = requests.get(url)
+
 html_contents = r.text
 
 html_soup = BeautifulSoup(html_contents, 'html.parser')
-# Find the first h1 tag
-first_h1 = html_soup.find('h1')
+first_h3 = html_soup.find('h3')
+print("name = ",first_h3.name) # h1
+print("content = ", first_h3.contents) # ['List of ', [...],]
 
+print("str()  =  ", str(first_h3))
 
-if first_h1:
-    print(first_h1.name)
-    print(first_h1.contents)
-else:
-    print("לא נמצאה תגית h1")
+print("text =", first_h3.text)
+ 
 
-print(first_h1.contents)    
+ 
