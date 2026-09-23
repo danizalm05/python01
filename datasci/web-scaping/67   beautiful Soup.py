2d0f259243fe609827492e548ef60dad1ec1ac18
danizalm05 <danizalm05@gmail.com>
2026-09-23 17:55:36 +0300

    17140

--- datasci/web-scaping/67   beautiful Soup.py
+++ datasci/web-scaping/67   beautiful Soup.py
@@ -2,19 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://linuxsimply.com/linux-basics/os-installation/virtual-machine/kali-linux-on-virtualbox/'
-url1 = 'https://news.walla.co.il/item/3868579'
+url1 = 'https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_episodes'
 r = requests.get(url)
 
 html_contents = r.text
-
+ 
 html_soup = BeautifulSoup(html_contents, 'html.parser')
-first_h3 = html_soup.find('h3')
-print("name = ",first_h3.name) # h1
-print("content = ", first_h3.contents) # ['List of ', [...],]
 
-print("str()  =  ", str(first_h3))
+tag= 'h3'#'h3'
 
-print("text =", first_h3.text)
+first_tag = html_soup.find(tag)
+print("name = ",first_tag.name) 
+print("content = ", first_tag.contents) # ['List of ', [...],]
+
+print("str()  =  ", str(first_tag))
+
+print("text =", first_tag.text)
+ 
+print('first_tag.span = ',first_tag.span)
+print( '\n id  =',first_tag.span['id']) # firstHeading
+
+
+print('\n\n------------ h3 ------------')
+# Find the first five h3 elements with a   class
+tag = html_soup.find_all('h3',  limit=2)
+ 
+print(tag)
  
 
- 
+'''
+<h3><span class="ez-toc-section" id="Step_2_Create_a_New_Virtual_Machine" ez-toc-data-id="#Step_2_Create_a_New_Virtual_Machine"></span>Step 2: Create a New Virtual Machine<span class="ez-toc-section-end"></span></h3>
+'''
