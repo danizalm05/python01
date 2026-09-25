9ceab8cf7ac99447c6608c416d0ba30b09ecf9ac
danizalm05 <danizalm05@gmail.com>
2026-09-25 17:00:00 +0300

    13482

--- datasci/web-scaping/67   beautiful Soup.py
+++ datasci/web-scaping/67   beautiful Soup.py
@@ -24,12 +24,31 @@ print( '\n id  =',first_tag.span['id']) # firstHeading
 
 
 print('\n\n------------ h3 ------------')
-# Find the first five h3 elements with a   class
-tag = html_soup.find_all('h3',  limit=2)
- 
-print(tag)
- 
 
 '''
-<h3><span class="ez-toc-section" id="Step_2_Create_a_New_Virtual_Machine" ez-toc-data-id="#Step_2_Create_a_New_Virtual_Machine"></span>Step 2: Create a New Virtual Machine<span class="ez-toc-section-end"></span></h3>
+ <h3>
+  <span class="ez-toc-section" 
+  id="Step_2_Create_a_New_Virtual_Machine" 
+  ez-toc-data-id="#Step_2_Create_a_New_Virtual_Machine"
+   ></span>
+   Step 2: Create a New Virtual Machine
+   <span class="ez-toc-section-end">
+   </span>
+  </h3>
+
 '''
+# Find    acorrding to id
+span = html_soup.find("span", id="Step_1_Download_Kali_Linux_ISO_File")
+
+if span:
+    h3 = span.find_parent("h3")
+    print(h3.get_text(" ", strip=True))
+    
+# Find    all h3 with  h3 span.ez-toc-section
+for span in html_soup.select("h3 span.ez-toc-section"):
+    h3 = span.parent
+    print(h3.get_text(" ", strip=True))
+
+#find just the first 4 items
+for h3 in html_soup.select("h3:has(span.ez-toc-section)")[:4]:
+    print(h3.get_text(" ", strip=True))
